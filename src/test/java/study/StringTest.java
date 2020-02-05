5c1cb9a70a007fb6f57ed882238558dc900f15d1
woonjangahn <woonjangahn@gmail.com>
2020-02-05 17:45:36 +0900

    TEST: 요구사항 3 분리 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,41 +7,48 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("String methods test")
 public class StringTest {
-	private String value;
+	private String input;
 	private String[] result;
 
 	@Test
 	@DisplayName("split() when delimiter exists within input")
 	void splitNumbers() {
-		value = "1,2";
-		result = value.split(",");
+		input = "1,2";
+		result = input.split(",");
 		assertThat(result).contains("1").contains("2");
 	}
 
 	@Test
 	@DisplayName("split() when delimiter does not exists within input")
 	void splitNumber() {
-		value = "1";
-		result = value.split(",");
+		input = "1";
+		result = input.split(",");
 		assertThat(result).containsExactly("1");
 	}
 
 	@Test
 	@DisplayName("substring() works properly")
 	void substring() {
-		value = "(1,2)";
-		String substringResult = value.substring(1, 4);
+		input = "(1,2)";
+		String substringResult = input.substring(1, 4);
 		assertThat(substringResult).isEqualTo("1,2");
+	}
+
+	@Test
+	@DisplayName("chatAt() works properly")
+	void charAtWorksProperly() {
+		input = "abc";
+		for (int i = 0; i < input.length(); i++) {
+			assertThat(input.charAt(i)).isInstanceOf(Character.class);
+		}
 	}
 
 	@Test
 	@DisplayName("charAt() throws StringIndexOutOfBoundsException")
 	void charAt() {
-		value = "abc";
+		input = "abc";
 		assertThatThrownBy(() -> {
-			for (int i = 0; i <= 3; i++) {
-				value.charAt(i);
-			}
+			input.charAt(input.length());
 		}).isInstanceOf(StringIndexOutOfBoundsException.class).hasMessageContaining("String index out of range: 3");
 	}
 }
