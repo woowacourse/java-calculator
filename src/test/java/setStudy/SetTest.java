63a3b72a99cffd90d266c8edc9d43d49d22ea829
Ramen6315 <ramen6315@gmail.com>
2020-02-05 17:58:57 +0900

    @BeforeEach를 이용해서 Set의 크기를 확인하는 학습 테스트
     - @BeforeEach의 용도를 확인하고 이것을 이용해서 Set size를 확인한다

---
+++ src/test/java/setStudy/SetTest.java
@@ -0,0 +1,28 @@
+package setStudy;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("numbers의 크기를 확인하는 학습 테스트")
+    void setSize() {
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+}
