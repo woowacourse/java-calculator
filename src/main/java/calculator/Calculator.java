0bb13ba914f23b0f0dcc53a176e360330c317a14
taejinseok <stj930@gmail.com>
2020-02-05 15:54:17 +0900

    [feat] 사칙연산을 수행하는 Calculator 추가

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+public class Calculator {
+	int	add(int number1, int number2) {
+		return number1 + number2;
+	}
+
+	int subtract(int number1, int number2) {
+		return number1 - number2;
+	}
+
+	int multiply(int number1, int number2) {
+		return number1 * number2;
+	}
+
+	int divide(int number1, int number2) {
+		if (number2 == 0) {
+			throw new IllegalArgumentException("0으로 나누지 마시오.");
+		}
+		return number1 / number2;
+	}
+}
