ed7ba12b3913c2cf33eb2c52d61f215ebd96b7a6
giantim <giantim132@gmail.com>
2020-02-05 15:06:08 +0900

    add : 요구사항 1 - split 했을 때 contains()를 활용해 변환 값이 맞는지 검증하는 테스트 코드 작성

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,16 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+
+public class StringTest {
+    @Test
+    void split() {
+        String value = "1,2";
+        String[] result = value.split(",");
+        Assertions.assertThat(result).contains("1");
+        Assertions.assertThat(result).contains("2");
+        String[] temp = {"1", "2"};
+        Assertions.assertThat(result).containsExactly(temp);
+    }
+}
