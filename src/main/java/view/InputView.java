f76d0961e2997a9152be313d18db7df9ed504d25
JunSeong <ebseud6135@naver.com>
2020-02-05 15:17:11 +0900

    문자열 계산기 - 입력받는 기능 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,10 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static String input() {
+        System.out.println("계산식을 입력하세요 : ");
+        return new Scanner(System.in).nextLine();
+    }
+}
