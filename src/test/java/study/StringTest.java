fd9aacffb4ad4d954de0ed1bff4e610f17d99167
chanin <pci2676@gmail.com>
2020-02-05 13:35:20 +0900

    String 클래스에 대한 학습테스트-3
    charAt()에 대한 범위 밖의 인자값 입력시 Exception test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringTest {
     @DisplayName("spilt()에 대한 test")
@@ -19,12 +20,26 @@ class StringTest {
 
     @DisplayName("substring() test")
     @Test
-    void name() {
+    void Two() {
         //given
         String string = "(1,2)";
         //when
         String result = string.substring(1, 4);
         //then
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt()에 대한 범위 밖의 인자값 입력시 Exception test")
+    @Test
+    void Three() {
+        //given
+        String string = "abc";
+        //when
+
+        assertThatThrownBy(() -> {
+            string.charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+        //then
     }
 }
