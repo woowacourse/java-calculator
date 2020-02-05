0ce73bb9e9d14e66c5cdeb8875bb989db5c6d69e
LTTTTTE <10100aaaaa@gmail.com>
2020-02-05 17:31:20 +0900

    feat : SetTest 클래스의 contains 기댓값 테스트 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -29,11 +30,20 @@ public class SetTest {
 		assertThat(numbers.size()).isEqualTo(3);
 	}
 
-
 	@ParameterizedTest
 	@ValueSource(ints = {1, 2, 3})
 	@DisplayName("Set contains 테스트")
 	public void contains(int number) {
 		assertThat(numbers).contains(number);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+	@DisplayName("Set contains 기댓값 테스트")
+	public void containsWithExpectedValue(String input, String expected) {
+		int actualInput = Integer.parseInt(input);
+		boolean actualExpected = Boolean.parseBoolean(expected);
+
+		assertThat(numbers.contains(actualInput)).isEqualTo(actualExpected);
 	}
 }
