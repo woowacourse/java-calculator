599da14935626a85b7859891b070b516d538dc20
LTTTTTE <10100aaaaa@gmail.com>
2020-02-05 17:01:19 +0900

    feat : StringTest 클래스의 charAt 테스트 코드 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,7 @@ package study;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -21,5 +22,16 @@ public class StringTest {
 		String result = value.substring(1, 4);
 
 		assertThat(result).isEqualTo("1,2");
+	}
+
+	@Test
+	@DisplayName("IndexOutOfBoundsException 발생 테스트")
+	public void charAt() {
+		String value = "abc";
+
+		assertThatExceptionOfType(IndexOutOfBoundsException.class)
+			.isThrownBy(() -> {
+				char c = value.charAt(4);
+			}).withMessageMatching("String index out of range: \\d+");
 	}
 }
