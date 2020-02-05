602e2add2de1d590d6c8b78d051ffbe35f505496
junk-sound <wom2277@naver.com>
2020-02-05 15:26:18 +0900

    test : CharAtSuccess

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
@@ -17,5 +18,12 @@ public class StringTest {
         String value = "(1,2)";
         String result = value.substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAtSuccess() {
+        String value = "abc";
+        char result = value.charAt(1);
+        assertThat(result).isEqualTo('b');
     }
 }
