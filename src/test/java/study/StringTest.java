954ffc88cfc5682d0d135df3c128f3d937e5d2ba
hwanghe159 <hwanghe159@gmail.com>
2020-02-05 17:44:56 +0900

    [String 클래스] 요구사항1 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,4 +7,13 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class StringTest {
+    @Test
+    void firstRequire() {
+        String stringWithOneTwo = "1,2";
+        String stringWithOnlyOne = "1";
+
+        Assertions.assertThat(stringWithOneTwo.split(",")).contains("1");
+        Assertions.assertThat(stringWithOneTwo.split(",")).contains("2");
+        Assertions.assertThat(stringWithOnlyOne.split(",")).containsExactly("1");
+    }
 }
