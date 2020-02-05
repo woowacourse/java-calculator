e175d574449f0d60499b7d0f487aa8315482c64d
kimjunseok <jnunseok@gmail.com>
2020-02-05 13:29:23 +0900

    [Add] substring method

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -13,4 +13,11 @@ public class StringTest {
         Assertions.assertThat(result).contains("1");
         Assertions.assertThat(result).contains("2");
     }
+
+    @Test
+    void substring() {
+        String value = "(1,2)";
+        String result = value.substring(1,4);
+        Assertions.assertThat(result).contains("1,2");
+    }
 }
