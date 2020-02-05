00b743c9b150d9e375325e0eeb9a0154d2d1993c
kimjunseok <jnunseok@gmail.com>
2020-02-05 17:11:05 +0900

    [Add] method to check size of Set

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2019 by tiger, DDoring
+ * All rights reserved.
+ *
+ * SetTest.java
+ * Set Collection에 대한 학습 테스트
+ *
+ * @author      tiger, DDoring
+ * @version     1.0
+ * @date        05 Feb 2020
+ *
+ */
+
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
