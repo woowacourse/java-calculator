2bb20b3dcd79a823e442f0375208cbed6c24dbc6
kimjunseok <jnunseok@gmail.com>
2020-02-05 13:28:21 +0900

    [Add] split method

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,16 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+    @Test
+    void split() {
+        String value = "1,2";
+        String[] result = value.split(",");
+        Assertions.assertThat(result).contains("1");
+        Assertions.assertThat(result).contains("2");
+    }
+}
