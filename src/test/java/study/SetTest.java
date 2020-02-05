58cc5d47995eb082e749a1172b9e7808fb4dc4e6
김예림 <kiel0103@naver.com>
2020-02-05 15:57:22 +0900

    SetTest 클래스 기본 메서드 작성
    SetTest할 Set 클래스 인스턴스 생성하고
    메서드 setUp() 작성

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,20 @@
+package study;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+}
