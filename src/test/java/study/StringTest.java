3d138c32f326a42954cde4bcd88f40855fcf27bd
hwangbo <taekwonod714@naver.com>
2020-02-05 16:34:52 +0900

    요구사항 1

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,21 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+//Class명을 제거하기 위해 static import를 한다.
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+    @Test
+    void split() {
+        // 요구사항 1
+//         String value = "1,2";
+//         String[] result = value.split(",");
+//         assertThat(result).contains("1");
+//         assertThat(result).contains("2");
+//         String value = "1";
+//         String[] result = value.split(",");
+//         assertThat(result).containsExactly("1");
+    }
+
+}
