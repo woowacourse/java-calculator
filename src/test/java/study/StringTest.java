3f266e210e9faa4ddb8407eb1c85809e3d989f35
hwangbo <taekwonod714@naver.com>
2020-02-05 17:29:49 +0900

    요구사항 3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,11 +1,13 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 //Class명을 제거하기 위해 static import를 한다.
 import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
+    @DisplayName("범위 테스트")
     @Test
     void split() {
         // 요구사항 1
@@ -16,10 +18,19 @@ public class StringTest {
 //         String value = "1";
 //         String[] result = value.split(",");
 //         assertThat(result).containsExactly("1");
+    }
+    void substring() {
         // 요구사항 2
 //         String value = "(1,2)";
 //         String result = value.substring(1, value.length() - 1);
 //         System.out.println(result);
+    }
+    void errorThrow() {
+        // 요구사항 3
+//        assertThatThrownBy(() -> {
+//                String val = "ab";
+//                val.charAt(2);
+//        }).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("2");
     }
 
 }
