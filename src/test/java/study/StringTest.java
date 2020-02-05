872f8260ee5e333ff780b04854b9f23e3fc3a823
JunSeong <ebseud6135@naver.com>
2020-02-05 13:49:49 +0900

    substring Test case 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -13,4 +13,12 @@ public class StringTest {
         assertThat(result).contains("1");
         assertThat(result).contains("2");
     }
+
+
+    @Test
+    void substring() {
+        String value = "(1,2)";
+        String result = value.substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
 }
