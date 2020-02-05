79a4720d52555edff1c936708c6791c97708f786
Tak Hyungmin <milkho11@naver.com>
2020-02-05 17:26:56 +0900

    Complete StringTest 요구사항 2
    학습 테스트를 위한 String에 대한 Assertion 사용법 숙지

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -16,4 +16,11 @@ public class StringTest {
         assertThat(result[0]).isEqualTo("1");   //actual 값과 expected 값이 같은지 확인
         assertThat(result[1]).isEqualTo("2");
     }
+
+    @Test
+    void subString() {
+        String value = "(1,2)";
+        String letter = value.substring(1, 4);
+        assertThat(letter).isEqualTo("1,2");
+    }
 }
