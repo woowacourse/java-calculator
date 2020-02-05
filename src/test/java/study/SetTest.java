44d8a8c425c2bdbb1ed881f24b92a04355c309cf
toneyparky <qkrtmddhks95@gmail.com>
2020-02-05 14:00:27 +0900

    Add: contains test method

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set numbers;
@@ -25,4 +29,12 @@ public class SetTest {
     void size() {
         Assertions.assertEquals(numbers.size(), 3);
     }
+
+    @ParameterizedTest
+    @DisplayName("contains test")
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertTrue(numbers.contains(number));
+    }
+
 }
