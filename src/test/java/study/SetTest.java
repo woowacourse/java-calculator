141809c51d5aad302eb948750830dad9b0e37c94
LTTTTTE <10100aaaaa@gmail.com>
2020-02-05 17:11:34 +0900

    feat : SetTest 클래스의 size 테스트 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,13 @@
 package study;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class SetTest {
 	private Set numbers;
@@ -15,5 +19,11 @@ public class SetTest {
 		numbers.add(1);
 		numbers.add(2);
 		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName("Set 사이즈 테스트")
+	public void size() {
+		assertThat(numbers.size()).isEqualTo(3);
 	}
 }
