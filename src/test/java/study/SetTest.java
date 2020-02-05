e9d0d07ddbbbf2ef4a82227cebcdc75f95f399a8
김예림 <kiel0103@naver.com>
2020-02-05 16:48:07 +0900

    Set의 contains() 메소드 테스트
    contains 메소드는 true, false 두가지의 결과값을 가질 수 있다.
    true가 나와야 하는 상황에 true가 잘 나오는지,
    false가 나와야 하는 상황에 false가 잘 나오는지에 대해
    테스트 메서드를 작성하였다.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,10 +2,12 @@ package study;
 
 import java.util.HashSet;
 import java.util.Set;
+import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -30,5 +32,12 @@ public class SetTest {
     @ValueSource(strings = {"1", "2", "3"})
     void contains(String input) {
         Assertions.assertThat(numbers.contains(Integer.parseInt(input))).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value= {"1:true", "2:true", "4:false"}, delimiter = ':')
+    void contains(String input, String expected) {
+        boolean actual = numbers.contains(Integer.parseInt(input));
+        Assertions.assertThat(String.valueOf(actual)).isEqualTo(expected);
     }
 }
