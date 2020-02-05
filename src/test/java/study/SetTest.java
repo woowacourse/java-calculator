c0bc2a38650f6fac888f173705a1546bc3ecfcb6
LTTTTTE <10100aaaaa@gmail.com>
2020-02-05 17:21:30 +0900

    feat : SetTest 클래스의 contains 테스트 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 	private Set numbers;
@@ -25,5 +27,13 @@ public class SetTest {
 	@DisplayName("Set 사이즈 테스트")
 	public void size() {
 		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	@DisplayName("Set contains 테스트")
+	public void contains(int number) {
+		assertThat(numbers).contains(number);
 	}
 }
