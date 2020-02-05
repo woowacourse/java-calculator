8284be7bcc8ec21be7b8cf55410a7a76d5c93655
JunSeong <ebseud6135@naver.com>
2020-02-05 13:52:42 +0900

    charAt + IndexOutOfBoundsException Test case 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,7 +1,9 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -20,5 +22,14 @@ public class StringTest {
         String value = "(1,2)";
         String result = value.substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt 테스트")
+    @Test
+    void charAt() {
+        String value = "abc";
+        assertThat(value.charAt(1)).isEqualTo('b');
+        assertThatThrownBy(() -> value.charAt(-1)).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range");
     }
 }
