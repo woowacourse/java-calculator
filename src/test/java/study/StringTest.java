2fd7456b6abd70638f5b1c7e51b4bade72d3af67
JunSeong <ebseud6135@naver.com>
2020-02-05 13:48:05 +0900

    split Test case 추가

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,16 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    void split() {
+        String value = "1,2";
+        String[] result = value.split(",");
+        assertThat(result).contains("1");
+        assertThat(result).contains("2");
+    }
+}
