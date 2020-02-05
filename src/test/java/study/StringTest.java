d56559ce80e1ac0dad2bcc12b0831987f8ea9a13
LTTTTTE <10100aaaaa@gmail.com>
2020-02-05 16:55:50 +0900

    feat : StringTest 클래스의 substring 테스트 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -9,8 +9,17 @@ public class StringTest {
 	void split() {
 		String value = "1,2";
 		String[] result = value.split(",");
+
 		assertThat(result).contains("1");
 		assertThat(result).contains("2");
 		assertThat(result).containsExactly("1", "2");
+	}
+
+	@Test
+	public void substring() {
+		String value = "(1,2)";
+		String result = value.substring(1, 4);
+
+		assertThat(result).isEqualTo("1,2");
 	}
 }
