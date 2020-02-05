52d1fa6ddd519b67c94a1921749345729f97c8fd
taejinseok <stj930@gmail.com>
2020-02-05 16:22:48 +0900

    [test] Calculator 사칙연산 기능

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,50 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CalculatorTest {
+	private Calculator calculator = new Calculator();
+
+	@ParameterizedTest
+	@CsvSource(value = {"5:3:8", "3:2:5"}, delimiter = ':')
+	void add(int number1, int number2, int expected) {
+		assertThat(calculator.add(number1, number2)).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"5:3:2", "3:2:1"}, delimiter = ':')
+	void subtract(int number1, int number2, int expected) {
+		assertThat(calculator.subtract(number1, number2)).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"5:3:15", "3:2:6"}, delimiter = ':')
+	void multiply(int number1, int number2, int expected) {
+		assertThat(calculator.multiply(number1, number2)).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"6:3:2", "3:1:3"}, delimiter = ':')
+	void divide(int number1, int number2, int expected) {
+		assertThat(calculator.divide(number1, number2)).isEqualTo(expected);
+	}
+
+	@Test
+	@DisplayName("Divide with 0 test")
+	void divideWithZero() {
+		assertThatThrownBy(() -> calculator.divide(10, 0))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("0으로 나누지 마시오.");
+	}
+}
