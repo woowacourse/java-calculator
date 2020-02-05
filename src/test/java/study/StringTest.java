69e0a00a81c99da8efe243e534e3cf7988b2f92d
KIMSIYOUNG <tldud2404@gmail.com>
2020-02-05 13:56:28 +0900

    feature : Add String requirement2 method

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -5,15 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
+
     @Test
     void split() {
         String value = "1,2";
         String[] split = value.split(",");
 
         assertThat(split.length).isEqualTo(2);
-        assertThat(split).contains("2","1");
+        assertThat(split).contains("2", "1");
         assertThat(split).doesNotContain(",");
-        assertThat(value.split(",")).containsExactly("1","2");
+        assertThat(value.split(",")).containsExactly("1", "2");
+    }
 
+    @Test
+    void substring() {
+        String value = "(1,2)";
+        String result = value.substring(1,4);
+        assertThat(result).isEqualTo("1,2");
+        assertThat(result).doesNotContain("(",")");
     }
 }
