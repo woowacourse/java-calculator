1024cac5bcbf7651ee50f452cdd8c63793c690cf
Sujin <sujinemilycho@gmail.com>
2020-02-05 17:48:31 +0900

    feat: 사칙연산 메서드 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -23,4 +23,20 @@ public class Calculator {
         String[] elements = inputString.split("\\s+", 0);
         return new LinkedList<>(Arrays.asList(elements));
     }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
 }
