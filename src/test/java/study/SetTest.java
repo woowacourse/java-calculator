14838bb241aa38c7e3f8438b2ca53956f0e120cd
MOON JINJU <mjj920930@gmail.com>
2020-02-05 14:58:08 +0900

    test: size 메소드의 학습 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void methodStudy_SizeOfSet_IsThree() {
+        int result = numbers.size();
+        assertThat(result).isEqualTo(3);
+    }
+}
