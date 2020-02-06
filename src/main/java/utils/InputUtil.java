6673ca3d7369538d7de100711049bbc7b1959f47
LTTTTTE <10100aaaaa@gmail.com>
2020-02-06 18:15:09 +0900

    feat : 사용자입력을 공백으로 잘라주는 기능 추가

--- src/main/java/utils/InputUtil.java
+++ src/main/java/utils/InputUtil.java
@@ -3,11 +3,29 @@ package utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputUtil {
+	private static final String SPACE = " ";
+	private static final String EMPTY = "";
+
 	private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
 	public static String inputMathematicalExpression() throws IOException {
-		return bufferedReader.readLine();
+		return checkException(bufferedReader.readLine());
+	}
+
+	private static String checkException(String input) {
+		return "";
+	}
+
+	private static String trimSpace(String input) {
+		return input.replaceAll(SPACE, EMPTY);
+	}
+
+	public static List<String> split(String input) {
+		return Stream.of(input.split(SPACE)).collect(Collectors.toList());
 	}
 }
