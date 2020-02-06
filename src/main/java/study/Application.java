2f835a46d5c76f3b6c85c84d47dd3fbd30f8f4ba
Andy H. Jung <commaniakr@gmail.com>
2020-02-06 19:36:07 +0900

    fix: 잘못된 식 예외 처리

--- src/main/java/study/Application.java
+++ src/main/java/study/Application.java
@@ -3,11 +3,23 @@ package study;
 import java.util.Scanner;
 
 public class Application {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("계산식을 입력하세요:");
-		String value = scanner.nextLine();
-		String[] values = value.split(" ");
-		System.out.println(Calculator.calculate(values));
-	}
+    public static void main(String[] args) {
+        runCalculator();
+    }
+
+    private static void runCalculator() {
+        try {
+            String[] values = inputExpression();
+            System.out.println("정답은 " + Calculator.calculate(values) + "입니다.");
+        } catch (NumberFormatException e) {
+            System.out.println("다시 입력해 주세요.");
+            runCalculator();
+        }
+    }
+
+    private static String[] inputExpression() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("계산식을 입력하세요:");
+        return scanner.nextLine().split(" ");
+    }
 }
