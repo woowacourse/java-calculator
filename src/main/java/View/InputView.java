95d50ea770e19a835487ea78e364d1e45b801e84
choiyeonseok <smr603@snu.ac.kr>
2020-02-06 13:14:33 +0900

    [StringCalculator] Add InputView class

---
+++ src/main/java/View/InputView.java
@@ -0,0 +1,13 @@
+package View;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static String input;
+
+    public static String[] input() {
+        Scanner sc = new Scanner(System.in);
+        input = sc.nextLine();
+        return input.split(" ");
+    }
+}
