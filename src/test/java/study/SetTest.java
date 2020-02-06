d92c9861105b4413719d2f1a541ee78c881ffe76
hwanghe159 <hwanghe159@gmail.com>
2020-02-06 14:51:26 +0900

    [SET 컬렉션] 요구사항3 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -33,5 +34,12 @@ public class SetTest {
     @DisplayName("parameterized test with contains")
     void testContains(Integer input) {
         Assertions.assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,false", "2,false", "3,false","4,true"})
+    @DisplayName("csv test")
+    void testMultiplyTwo(String input, String expected) {
+        Assertions.assertThat(numbers.add(Integer.parseInt(input))).isEqualTo(Boolean.parseBoolean(expected));
     }
 }
