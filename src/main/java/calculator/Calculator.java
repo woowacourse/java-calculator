bfbd0f5b1f00a3b4e7464d852959ac0f7a0ea9b6
Sujin <sujinemilycho@gmail.com>
2020-02-06 18:16:05 +0900

    feat : 입력에 대한 계산을 요청하는 메서드 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,6 +5,10 @@ import java.util.*;
 public class Calculator {
     private Set opertors;
     private Queue<String> equation;
+    private final static String plus = "+";
+    private final static String minus = "-";
+    private final static String multiply = "*";
+    private final static String divide = "/";
 
     public Calculator() {
         opertors = new HashSet<>();
@@ -22,6 +26,31 @@ public class Calculator {
     public Queue makeQueue(String inputString) {
         String[] elements = inputString.split("\\s+", 0);
         return new LinkedList<>(Arrays.asList(elements));
+    }
+
+    public int execute() {
+        int result = Integer.parseInt(equation.poll());
+        while (!equation.isEmpty()) {
+            String operator = equation.poll();
+            int operand = Integer.parseInt(equation.poll());
+            result = calculate(result, operator, operand);
+        }
+        return result;
+    }
+
+    public int calculate(int result, String operator, int operand) {
+        switch (operator) {
+            case plus:
+                return add(result, operand);
+            case minus :
+                return subtract(result, operand);
+            case multiply :
+                return multiply(result, operand);
+            case divide :
+                return divide(result, operand);
+            default :
+                throw new InputMismatchException("해당 연산자 없음");
+        }
     }
 
     public int add(int a, int b) {
