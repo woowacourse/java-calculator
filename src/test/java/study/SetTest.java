f82e316c1eda1f2b6c45abe1de6d4eaecb9d197e
hwanghe159 <hwanghe159@gmail.com>
2020-02-06 14:19:00 +0900

    [Set 컬렉션] 요구사항1 구현

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("test set size")
+    void testSetSize(){
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+}
