d21b05be49e81165945d990c03239e17b5d0e417
Tak Hyungmin <milkho11@naver.com>
2020-02-06 13:24:09 +0900

    Complete SetTest 요구사항 2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,15 +1,15 @@
 package study;
 
-import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
 
 public class SetTest {
     private Set numbers;
@@ -27,5 +27,12 @@ public class SetTest {
     @Test
     void size() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("중복 값 발생 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 2, 1})
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 }
