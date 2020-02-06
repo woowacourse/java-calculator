3547d5dc23171f15eb55fb14af2ba9923eb2527a
hongbin <khbasd@gmail.com>
2020-02-06 21:45:53 +0900

    test: calculate Test

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,35 +7,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-    Calculator controller = new Calculator();
-    String formula;
-
-    @BeforeAll
-    void setUp() {
-        formula = "3 + 2 - 1";
-    }
+    Calculator calculator = new Calculator();
 
     @DisplayName("계산식 분할 테스트")
     @Test
     void splitFormula() {
+        String formula = "3 + 2 - 1";
         double[] numbers = {3, 2, 1};
         char[] operators = {'+', '-'};
 
-        controller.splitFormula(formula);
+        calculator.splitFormula(formula);
 
-        double[] targetNumbers = controller.getNumbers().stream().mapToDouble(Double::doubleValue).toArray();
+        double[] targetNumbers = calculator.getNumbers().stream().mapToDouble(Double::doubleValue).toArray();
         Assertions.assertThat(numbers).isEqualTo(targetNumbers);
 
-        char[] targetOperators = new char[controller.getOperators().size()];
-        for (int i = 0; i < controller.getOperators().size(); i++) {
-            targetOperators[i] = controller.getOperators().get(i);
+        char[] targetOperators = new char[calculator.getOperators().size()];
+        for (int i = 0; i < calculator.getOperators().size(); i++) {
+            targetOperators[i] = calculator.getOperators().get(i);
         }
         Assertions.assertThat(operators).isEqualTo(targetOperators);
     }
 
     @DisplayName("컬랙션 분할 후 계산 테스트")
     @Test
-    public void runCalculator() {
+    public void calculate() {
+        String formula = "3 + 2 - 1";
+        calculator.splitFormula(formula);
 
+        double result = calculator.calculate();
+        Assertions.assertThat(result).isEqualTo(4);
     }
 }
