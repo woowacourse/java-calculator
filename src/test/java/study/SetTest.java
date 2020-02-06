80746fae6f1b1de50669ca1e7402130b6495432e
Tak Hyungmin <milkho11@naver.com>
2020-02-06 13:40:03 +0900

    Complete SetTest 요구사항 3

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -34,5 +36,14 @@ public class SetTest {
     @ValueSource(ints = {3, 2, 1})
     void contains(int input) {
         assertThat(numbers.contains(input)).isTrue();
+        Iterator<Integer> it = numbers.iterator();
+
+    }
+
+    @DisplayName("입력에 따른 결과값이 다른 경우 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1:0", "2:1", "3:1"}, delimiter = ':')
+    void containsTo(int actual, int expected) {
+        assertThat(actual).isGreaterThan(expected);
     }
 }
