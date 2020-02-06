e3eae490be37efb1090e8f1392dccaeb709d35de
김예림 <kiel0103@naver.com>
2020-02-06 19:45:53 +0900

    기능: 사용자에게 입력값을 받아 계산 결과를 보여주는 Runner 클래스 작성

---
+++ src/main/java/calculator/Runner.java
@@ -0,0 +1,37 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Runner {
+    Expression expression;
+    final int ONE_TIME_INPUT_SUCCESS = 1;
+    final int ONE_TIME_INPUT_FAILED = 2;
+
+    public void run() {
+
+        // 입력값이 조건에 맞지 않는 경우 사용자에게 다시 입력을 요청함
+        while (oneTimeInput(getInput()) == ONE_TIME_INPUT_FAILED);
+
+        int result = Calculator.calculate(expression);
+
+        View.printResult(result);
+    }
+
+    private String getInput() {
+        System.out.println("계산할 문자열을 입력해 주세요: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private int oneTimeInput(String inputString) {
+
+        try {
+            expression = new Expression(inputString);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return ONE_TIME_INPUT_FAILED;
+        }
+
+        return ONE_TIME_INPUT_SUCCESS;
+    }
+}
