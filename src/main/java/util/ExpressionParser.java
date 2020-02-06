79054735b65fc716a969b139829b34e07405c9fa
Sony <sonypark0204@gmail.com>
2020-02-06 17:02:07 +0900

    feat: 특정항이 숫자인지 판별하는 기능 구현
    숫자는 음수인 경우도 포함한다.

--- src/main/java/util/ExpressionParser.java
+++ src/main/java/util/ExpressionParser.java
@@ -4,4 +4,30 @@ public class ExpressionParser {
 	public static String[] splitWithBlank(String expression) {
 		return expression.split(" ");
 	}
+
+	public static int stringToInt(String term) {
+		return Integer.parseInt(term);
+	}
+
+	public static boolean isNumber(String term) {
+		int firstIndex = 0;
+
+		if (isFirstNumberMinus(term)) {
+			firstIndex = 1;
+		}
+
+		for (int i = firstIndex; i < term.length(); i++) {
+			if (Character.isDigit(term.charAt(i)) == false) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean isFirstNumberMinus(String term) {
+		if (term.startsWith("-") && term.length() > 1) {
+			return true;
+		}
+		return false;
+	}
 }
