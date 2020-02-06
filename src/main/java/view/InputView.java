64f7e3960cf9e99cc14b8141d55a7a554d024f28
kimjunseok <jnunseok@gmail.com>
2020-02-06 15:45:16 +0900

    [Add] function to get string from user

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2019 by tiger,DDoring
+ * All rights reserved.
+ *
+ * InputView.java
+ * 사용자의 입력을 담당하는 클래스
+ *
+ * @author      tiger,DDoring
+ * @version     1.0
+ * @date        06 Feb 2020
+ *
+ */
+
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public String inputStringFromUser(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
