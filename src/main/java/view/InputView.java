e351fc22007daf534b0532800a941487096855d0
hongbin <khbasd@gmail.com>
2020-02-06 18:25:28 +0900

    feat: Edit InputView, OutputView
    InputView : next -> nextLine
    OutputView : add public

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,6 +6,6 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
     public static String inputFormula() {
-        return scanner.next();
+        return scanner.nextLine();
     }
 }
