1210b04d6e8bd533b1152fa63957460f9f5196ba
hongbin <khbasd@gmail.com>
2020-02-06 21:35:00 +0900

    feat: Add Calculator.calculate
    Add
    Member BiFunction, Map
    Constructor
    Method calculate

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -4,8 +4,18 @@ import view.InputView;
 import view.OutputView;
 
 import java.util.*;
+import java.util.function.BiFunction;
 
 public class Calculator {
+
+    private final List<Double> numbers = new ArrayList<>();
+    private final List<Character> operators = new ArrayList<>();
+    private final BiFunction<Double, Double, Double> ADDITION = (a, b) -> a + b;
+    private final BiFunction<Double, Double, Double> SUBTRACTION = (a, b) -> a + b;
+    private final BiFunction<Double, Double, Double> MULTIPLICATION = (a, b) -> a + b;
+    private final BiFunction<Double, Double, Double> DIVISION = (a, b) -> a + b;
+    private final Map<Character, BiFunction<Double, Double, Double>> calculateFunctionMap = new HashMap<>();
+
 
     public List<Double> getNumbers() {
         return numbers;
@@ -15,12 +25,16 @@ public class Calculator {
         return operators;
     }
 
-    private final List<Double> numbers = new ArrayList<>();
-    private final List<Character> operators = new ArrayList<>();
+    public Calculator() {
+        calculateFunctionMap.put('+', ADDITION);
+        calculateFunctionMap.put('-', SUBTRACTION);
+        calculateFunctionMap.put('*', MULTIPLICATION);
+        calculateFunctionMap.put('/', DIVISION);
+    }
 
     public void run() {
         splitFormula(InputView.inputFormula());
-        OutputView.printResult(runCalculator());
+        OutputView.printResult(calculate());
     }
 
     public void splitFormula(String formula) {
@@ -34,9 +48,11 @@ public class Calculator {
         }
     }
 
-    public double runCalculator() {
+    public double calculate() {
         double result = numbers.remove(0);
-
+        for (char operator : operators) {
+            result = calculateFunctionMap.get(operator).apply(result, numbers.remove(0));
+        }
         return result;
     }
 }
