76796616d0b82822b9fa1e9dcdcb0ac467dd537e
hongbin <khbasd@gmail.com>
2020-02-06 13:59:17 +0900

    feat: Add member var, method
    Add member var : numbers, operators
    Add method : splitFormula, runCalculator

--- src/main/java/domain/Controller.java
+++ src/main/java/domain/Controller.java
@@ -2,11 +2,30 @@ package domain;
 
 import view.InputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
 
     private final String formula;
+    private final List<Double> numbers = new ArrayList<>();
+    private final List<Character> operators = new ArrayList<>();
 
     public Controller() {
         formula = InputView.inputFormula();
+        // 스플릿
+        // 계산
+        // 출력
     }
+
+    private void splitFormula() {
+
+    }
+
+    private double runCalculator() {
+        double result = 0;
+
+        return result;
+    }
+
 }
