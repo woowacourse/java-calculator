71fa31acde41e8d4f697180df9ac49d97a46d4ef
Ramen6315 <ramen6315@gmail.com>
2020-02-06 14:15:46 +0900

    ParameterizedTest를 이용해서 검증 과정에서 중복을 제거 하는 학습

--- src/test/java/setStudy/SetTest.java
+++ src/test/java/setStudy/SetTest.java
@@ -4,6 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -24,5 +28,19 @@ public class SetTest {
     @DisplayName("numbers의 크기를 확인하는 학습 테스트")
     void setSize() {
         Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("numbers에 포함이 되어있을경우 True를 반환하는것을 확인하는 학습테스트 ")
+    void isContains_ShouldReturnTrueForSetElement(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4,5,6,7})
+    @DisplayName("numbers에 포함이 안되있는경우 False를 반환하는것을 확인하는 학습테스트 ")
+    void isContains_ShouldReturnFalseForSetElement(int input) {
+        assertThat(numbers.contains(input)).isFalse();
     }
 }
