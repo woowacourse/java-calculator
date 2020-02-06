9eff02d38de0e465b75a0146422406925cc1a6d3
hwanghe159 <hwanghe159@gmail.com>
2020-02-06 14:37:31 +0900

    [set 컬렉션] 요구사항2 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -22,7 +24,14 @@ public class SetTest {
 
     @Test
     @DisplayName("test set size")
-    void testSetSize(){
+    void testSetSize() {
         Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("parameterized test with contains")
+    void testContains(Integer input) {
+        Assertions.assertThat(numbers.contains(input)).isTrue();
     }
 }
