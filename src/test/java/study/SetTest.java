99b596bb10e1c1a2071c608745708c42db715d93
junk-sound <wom2277@naver.com>
2020-02-06 15:50:23 +0900

    test : Set contains - methodSource

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,16 +4,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SetTest {
     private Set<Integer> numbers;
     private List<Integer> initNumbers = Arrays.asList(1,1,2,3);
+    String[] aa = {"1,2"};
 
     @BeforeEach
     void setUp() {
@@ -24,6 +28,7 @@ class SetTest {
     @Test
     @DisplayName("#size() : return size of distict initNumbers")
     void size() {
+
         int expectedSize = initNumbers.stream()
                 .distinct()
                 .collect(Collectors.toList())
@@ -39,6 +44,21 @@ class SetTest {
     @DisplayName("#contains() : return true if set contains valueSource")
     void contains(Integer input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
 
+    @ParameterizedTest
+    @MethodSource({"getCompareableNumbers"} )
+    void containsMethod(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+
+    //todo: inject parameter
+    private static Stream<Arguments> getCompareableNumbers() {
+        return Stream.of(
+                Arguments.of(1,true),
+                Arguments.of(2,true),
+                Arguments.of(3,true),
+                Arguments.of(4,false)
+        );
     }
 }
