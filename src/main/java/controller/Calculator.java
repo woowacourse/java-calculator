2e5329439e04db5996aa83f72a1a3ff0bcb994e8
hongbin <khbasd@gmail.com>
2020-02-06 13:39:02 +0900

    feat: Add Calculator class

---
+++ src/main/java/controller/Calculator.java
@@ -0,0 +1,23 @@
+package controller;
+
+public class Calculator {
+    public Calculator() {
+
+    }
+
+    public double addition(double a, double b) {
+        return a + b;
+    }
+
+    public double subtraction(double a, double b) {
+        return  a - b;
+    }
+
+    public double multiplication(double a, double b) {
+        return  a * b;
+    }
+
+    public double division(double a, double b) {
+        return  a / b;
+    }
+}
