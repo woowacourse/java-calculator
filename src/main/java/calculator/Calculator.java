b62a1deae185726d7d9ba04d0980000ba295d6b4
Ramen6315 <ramen6315@gmail.com>
2020-02-06 16:03:59 +0900

    연산자를 구분해서 계산하는 기능 구현
     - 사칙 연산자에 따른 계산 결과를 저장한다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,6 +3,9 @@ package calculator;
 import java.util.Scanner;
 
 public class Calculator {
+    private static final int INITIAL_VALUE = 0;
+    private static final int CHECK_EVEN_NUMBER_MODULAR = 2;
+    private static final int EVEN_NUMBER = 1;
     public void main(String[] args) {
         Calculator calculator = new Calculator();
         calculator.run();
@@ -10,7 +13,26 @@ public class Calculator {
 
     private void run() {
         String[] equation = getEquation();
+        calculateEquation(equation);
     }
+
+    private void calculateEquation(String[] equation) {
+        int result = Integer.parseInt(equation[0]);
+        for(int i=0; i<equation.length; i++) {
+            if(i%CHECK_EVEN_NUMBER_MODULAR == EVEN_NUMBER) {
+                result = checkOperatorAndCalculate(result, equation[i], Integer.parseInt(equation[i+1]));
+            }
+        }
+    }
+
+    private int checkOperatorAndCalculate(int previous, String operator, int next) {
+        if(operator.equals("+")) { return previous + next; }
+        if(operator.equals("-")) { return previous - next; }
+        if(operator.equals("*")) { return previous * next; }
+        if(operator.equals("/")) { return previous / next; }
+        return -1;
+    }
+
 
     private String[] getEquation() {
         Scanner scanner = new Scanner(System.in);
