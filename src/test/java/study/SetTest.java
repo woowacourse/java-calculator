e537bd49254f3e892f48796427166a1f178fc92a
Tak Hyungmin <milkho11@naver.com>
2020-02-06 13:02:27 +0900

    Complete SetTest 요구사항 1

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,31 @@
+package study;
+
+import org.assertj.core.data.Percentage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
+
+public class SetTest {
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size 메소드 연습")
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
