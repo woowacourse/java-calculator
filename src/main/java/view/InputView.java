8aef2be2c4fa72e6eb195658ee5dc7cc8c1d7967
hongbin <khbasd@gmail.com>
2020-02-06 13:33:19 +0900

    feat: Add InputView class

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static Scanner scanner = new Scanner(System.in);
+
+    static String inputFormula() {
+        return scanner.next();
+    }
+}
