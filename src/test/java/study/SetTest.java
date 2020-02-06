b099da6d82ea69483357bdf53d94e6702a560fec
jaeju.jang <jjj0611@gmail.com>
2020-02-06 13:33:21 +0900

    test. set collection의 size 메서드 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,13 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class SetTest {
 
@@ -17,4 +21,11 @@ public class SetTest {
        numbers.add(2);
        numbers.add(3);
     }
+
+    @Test
+    @DisplayName("size()를 호출하면 set에 담겨 있는 요소의 개수를 반환")
+    void testSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
 }
