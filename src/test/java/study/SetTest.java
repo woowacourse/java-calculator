59f819b2d2ac4ba514633ae3d789ced22ae608d7
jaeju.jang <jjj0611@gmail.com>
2020-02-06 13:36:31 +0900

    test. parameterizedTest와 valueSource를 활용해 set collection의 contains메서드 테스트 변경

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -28,10 +30,10 @@ public class SetTest {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
-    @Test
-    void contains() {
-        assertThat(numbers.contains(1)).isTrue();
-        assertThat(numbers.contains(2)).isTrue();
-        assertThat(numbers.contains(3)).isTrue();
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains(number)를 호출하면 number가 set에 있으면 True를 반환")
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
