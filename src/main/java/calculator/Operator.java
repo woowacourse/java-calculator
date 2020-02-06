c767c9f8c77842de4924e573ac0649f64b3e5306
김예림 <kiel0103@naver.com>
2020-02-06 19:32:51 +0900

    기능: Operator 클래스에 클래스 상수와 getType() 메서드 추가
    Calculator 클래스에서 계산 시 어떤 연산자를 사용했는지 알려주기 위해 추가

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,9 +1,31 @@
 package calculator;
 
 public class Operator {
+
+    public static final int ADD_TYPE = 1;
+    public static final int SUBTRACT_TYPE = 2;
+    public static final int MULTIPLY_TYPE = 3;
+    public static final int DIVIDE_TYPE = 4;
     private String value;
 
     public Operator(String input) {
         value = input;
+    }
+
+    public int getType() {
+
+        if (value.equals("+")) {
+            return ADD_TYPE;
+        }
+
+        if (value.equals("-")) {
+            return SUBTRACT_TYPE;
+        }
+
+        if (value.equals("*")) {
+            return MULTIPLY_TYPE;
+        }
+
+        return DIVIDE_TYPE;
     }
 }
