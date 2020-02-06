b0330fa18b1a9bf18339f69c439d2319842feb60
김예림 <kiel0103@naver.com>
2020-02-06 18:06:55 +0900

    기능: 수식(Expression) 클래스 제작
    - 사용자가 입력한 문자열이 수식에 적합한지 검사하여
    적합하면 인스턴스를 정상적으로 생성
    - 클래스 이름을 UserInput에서 Expression으로 변경
    - 기존의 미완성 메서드 내용을 완성함

---
+++ src/main/java/calculator/Expression.java
@@ -0,0 +1,100 @@
+package calculator;
+
+public class Expression {
+    private String[] expression;
+
+    public Expression(String inputString) {
+        String[] inputStringArray = splitInput(inputString);
+
+        if (!isCorrectStringArray(inputStringArray)) {
+            throw new IllegalArgumentException("입력은 다음과 같은 규칙을 따라야 합니다.\n"
+                + " * 1번째, 3번째, 5번째 등 홀수번 째 문자는 숫자여야합니다.\n"
+                + " * 2번째, 4번째, 6번째 등 짝수번 째 문자는 연산자(+, -, *, /)여야 합니다.\n"
+                + " * 마지막 문자는 숫자여야 합니다.\n"
+                + " * 각 문자들 사이에는 한 칸의 공백이 존재해야 하며, "
+                + "문장의 시작과 끝에는 공백이 없어야 합니다.\n");
+        }
+
+        this.expression = inputStringArray;
+    }
+
+    public String[] splitInput(String input) {
+        return input.split(" ");
+    }
+
+    public boolean isCorrectStringArray(String[] input) {
+        return isOperatorAtOddIndex(input) && isNumberAtEvenIndex(input) && isOddLength(input);
+    }
+
+    public boolean isOperatorAtOddIndex(String[] input) {
+        boolean ret = true;
+
+        for (int i = 1; i < input.length && ret == true; i += 2) {
+            ret = isOperator(input[i]);
+        }
+
+        return ret;
+    }
+
+    private boolean isOperator(String input) {
+        return input.equals("+") || input.equals("-") || input.equals("*") || input.equals("/");
+    }
+
+    public boolean isNumberAtEvenIndex(String[] input) {
+        boolean ret = true;
+
+        for (int i = 0; i < input.length && ret == true; i += 2) {
+            ret = isNumber(input[i]);
+        }
+
+        return ret;
+    }
+
+    private boolean isNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isOddLength(String[] input) {
+        return input.length % 2 == 1;
+    }
+
+    public int getExpressionLength() {
+        return expression.length;
+    }
+
+    public Operand getOperand(int position) {
+        int indexByPosition = getOperandIndexByPosition(position);
+
+        if (indexByPosition >= expression.length || indexByPosition < 0) {
+            return null;
+        }
+
+        return new Operand(expression[indexByPosition]);
+    }
+
+    private int getOperandIndexByPosition(int position) {
+        return (position - 1) * 2;
+    }
+
+    public Operator getOperator(int position) {
+        int indexByPosition = getOperatorIndexByPosition(position);
+
+        if (indexByPosition >= expression.length || indexByPosition <= 0) {
+            return null;
+        }
+
+        return new Operator(expression[indexByPosition]);
+    }
+
+    private int getOperatorIndexByPosition(int position) {
+        return (position - 1) * 2 + 1;
+    }
+
+
+}
