b6ac01b6b795d5587791d852467435177dc19a07
choiyeonseok <smr603@snu.ac.kr>
2020-02-06 18:42:54 +0900

    [StringCalculator] Add initializeInputs method

--- src/main/java/Controller/StringCalculator.java
+++ src/main/java/Controller/StringCalculator.java
@@ -1,10 +1,24 @@
 package Controller;
 
+import View.InputView;
+import View.OutputView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class StringCalculator {
     private List<Integer> numbers = new ArrayList<>();
     private List<Operator> operators = new ArrayList<>();
+    private double result;
 
+    public void initializeInputs() {
+        String[] inputs = InputException.checkException();
+        for (int i = 0; i < inputs.length; i++) {
+            try {
+                numbers.add(Integer.parseInt(inputs[i]));
+            } catch (Exception e){
+                operators.add(Operator.getEnumFromString(inputs[i]));
+            }
+        }
+    }
 }
