181e36c355f1d5c25ee79104156267b1794785c1
Ramen6315 <ramen6315@gmail.com>
2020-02-06 14:48:43 +0900

    ParameterizedTest, CsvSource annotation 학습 테스트
     - 각 Set element의 input값에 따라 결과 값이 다른 경우를 확인

--- src/test/java/setStudy/SetTest.java
+++ src/test/java/setStudy/SetTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set numbers;
@@ -31,16 +32,24 @@ public class SetTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1,2,3})
     @DisplayName("numbers에 포함이 되어있을경우 True를 반환하는것을 확인하는 학습테스트 ")
+    @ValueSource(ints = {1,2,3})
     void isContains_ShouldReturnTrueForSetElement(int input) {
         assertThat(numbers.contains(input)).isTrue();
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {4,5,6,7})
     @DisplayName("numbers에 포함이 안되있는경우 False를 반환하는것을 확인하는 학습테스트 ")
+    @ValueSource(ints = {4,5,6,7})
     void isContains_ShouldReturnFalseForSetElement(int input) {
         assertThat(numbers.contains(input)).isFalse();
     }
+
+    @ParameterizedTest
+    @DisplayName("각 Set element의 input의 contain 여부를 확인하는 학습 테스트")
+    @CsvSource(value = {"1, True", "4, False", "5, False"})
+    void isContains_EachCheckEachElement(int input, boolean expected){
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+
 }
