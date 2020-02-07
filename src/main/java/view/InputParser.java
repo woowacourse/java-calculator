9c84dcb967b0688181758de3f08a14361ed074a1
Tak Hyungmin <milkho11@naver.com>
2020-02-07 12:14:33 +0900

    [기능] 입력 문자 공백 기준 분리
    InputParser는 입력 수식을 숫자와 문자로 나누는 책임을 가진다.

---
+++ src/main/java/view/InputParser.java
@@ -0,0 +1,31 @@
+package view;
+
+import java.util.ArrayList;
+
+public class InputParser {
+    private final static String SPACE = " ";
+
+    public static String[] split(String input) {
+        return input.split(SPACE);
+    }
+
+    public static ArrayList<String> extractNumber(String[] input) {
+        ArrayList<String> numbers = new ArrayList<>();
+        for (int i = 0; i < input.length; i++) {
+            if (i % 2 == 0) {
+                numbers.add(input[i]);
+            }
+        }
+        return numbers;
+    }
+
+    public static ArrayList<String> extractOperator(String[] input) {
+        ArrayList<String> operators = new ArrayList<>();
+        for (int i = 0; i < input.length; i++) {
+            if (i % 2 != 0) {
+                operators.add(input[i]);
+            }
+        }
+        return operators;
+    }
+}
--- src/main/java/view/StringSpliter.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-public class StringSpliter {
-    private final static String SPACE = " ";
-
-    public static String[] split(String input) {
-        return input.split(SPACE);
-    }
-}
