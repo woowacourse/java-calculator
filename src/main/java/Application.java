9391f0abc0d8a36630bcf75a99a5d5e5a6e4f883
JunSeong <ebseud6135@naver.com>
2020-02-07 17:43:00 +0900

    문자열 계산기 - Main에서 입력과 try catch만 하도록 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,42 +5,18 @@ import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        startCalculate();
+        int result = calculateByInput();
+        OutputView.printResult(result);
     }
 
-    private static void startCalculate() {
+    private static int calculateByInput() {
         try {
-            OutputView.printResult(calculate());
+            Formulas formulas = new Formulas(InputView.input());
+            Calculator calculator = new Calculator(formulas);
+            return calculator.startCalculate();
         } catch (IllegalArgumentException e) {
             OutputView.exceptionMessage(e.getMessage());
-            startCalculate();
+            return calculateByInput();
         }
-    }
-
-    private static int calculate() {
-        Formulas formulas = new Formulas(InputView.input());
-        double result = checkNumbers(formulas.get(0));
-        for (int index = 1; index < formulas.size(); index += 2) {
-            result = Calculator.calculate(checkOperator(formulas.get(index)),
-                    result, checkNumbers(formulas.get(index + 1)));
-        }
-        return (int) result;
-    }
-
-    private static double checkNumbers(String number) {
-        if (!number.matches("^[0-9\\+\\-]?[0-9]+$")) {
-            throw new IllegalArgumentException("잘못된 계산식 - 피연산자가 숫자가 아님 : " + number);
-        }
-        if (Long.parseLong(number) > Integer.MAX_VALUE || Long.parseLong(number) < Integer.MIN_VALUE) {
-            throw new IllegalArgumentException("잘못된 계산식 - 피연산자가 INT 범위를 벗어남 : " + number);
-        }
-        return Double.parseDouble(number);
-    }
-
-    private static String checkOperator(String operator) {
-        if (operator.matches("^[\\+\\-\\*\\/]$")) {
-            return operator;
-        }
-        throw new IllegalArgumentException("잘못된 계산식 - 연산자가 잘못됨 : " + operator);
     }
 }
