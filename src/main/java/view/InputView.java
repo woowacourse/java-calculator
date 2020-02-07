056278f0c3692ab67eabae210110bdd9f79f56ea
junk-sound <wom2277@naver.com>
2020-02-07 17:00:32 +0900

    feat : Validation logic of input

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -40,7 +40,11 @@ public class InputView {
         return new NumberSentence(numbers, operators);
     }
 
+    //todo: 숫자 위치에 숫자가 아닌 입력이 있는 경우, 연산자 위치에 연산자가 아닌 경우 로직 추가
     private boolean isAbleToParse(String sentence) {
+        if (sentence.isEmpty()) {
+            return false;
+        }
         return true;
     }
 }
