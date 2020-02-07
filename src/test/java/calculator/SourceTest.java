5d6de179eedebc88e0acc9c1281e7208e1771a35
MOON JINJU <mjj920930@gmail.com>
2020-02-07 17:19:14 +0900

    test: 문자열 배열의 원소 개수가 홀수인지 검증하는 기능의 테스트 구현

--- src/test/java/calculator/SourceTest.java
+++ src/test/java/calculator/SourceTest.java
@@ -1,4 +1,27 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class SourceTest {
+    private Source source = new Source();
+
+    @BeforeEach
+    void setUp() {
+        String input = "1 + 2 +";
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1 + 2 +", "1 / 3 + 4 *", "4 4"})
+    void sizeOfInputs_ShouldBeOdds(String input) {
+        System.out.println(input);
+        String[] inputs = input.split(" ");
+        assertThatThrownBy(() -> {
+            source.validateSizeOfInputsIsOdds(inputs);
+        }).isInstanceOf(Exception.class)
+                .hasMessageMatching("inputs의 사이즈가 홀수가 아님");
+    }
 }
