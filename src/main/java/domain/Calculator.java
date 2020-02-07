6d0ceb608e6a27dadd9ebbebdd2fbb11212fbe50
lalize <printf@kakao.com>
2020-02-07 17:07:59 +0900

    refactor: values -> tokens 네이밍 수정

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -1,11 +1,11 @@
 package domain;
 
 public class Calculator {
-	public static int calculate(String[] values) {
-		int result = Integer.parseInt(values[0]);
-		for (int i = 1; i < values.length; i += 2) {
-			String operator = values[i];
-			int rightOperand = Integer.parseInt(values[i + 1]);
+	public static int calculate(String[] tokens) {
+		int result = Integer.parseInt(tokens[0]);
+		for (int i = 1; i < tokens.length; i += 2) {
+			String operator = tokens[i];
+			int rightOperand = Integer.parseInt(tokens[i + 1]);
 			result = Operator.of(operator)
 					.calculate(result, rightOperand);
 		}
