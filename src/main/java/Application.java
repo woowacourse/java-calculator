178592f340394a9bd1ef93258c2dd84427a360d8
woonjangahn <woonjangahn@gmail.com>
2020-02-07 17:09:58 +0900

    FEAT: 결과 출력 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,7 @@ public class Application {
 	public static void main(String[] args) {
 		InputHandler inputHandler = new InputHandler();
 		do {
-			new Calculator().run(inputHandler.getInput());
+			System.out.println("정답은 " + new Calculator().run(inputHandler.getInput()) + " 입니다.");
 		} while (inputHandler.isStartingAgain());
 	}
 }
