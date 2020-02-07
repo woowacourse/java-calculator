4b18029f947aa1e02eca6100ff890fa7da0d5859
Andy H. Jung <commaniakr@gmail.com>
2020-02-07 17:15:55 +0900

    fix: NullPointer 예외 처리

--- src/main/java/study/Application.java
+++ src/main/java/study/Application.java
@@ -21,11 +21,11 @@ public class Application {
         try {
             String[] values = inputExpression();
             System.out.println("정답은 " + Calculator.calculate(values) + "입니다.");
-        } catch (NumberFormatException e) {
-            System.out.println("다시 입력해 주세요.");
-            runCalculator();
         } catch (ArithmeticException e) {
             System.out.println("0으로 나눌 수 없습니다.");
+            runCalculator();
+        } catch (Exception e) {
+            System.out.println("다시 입력해 주세요.");
             runCalculator();
         }
     }
