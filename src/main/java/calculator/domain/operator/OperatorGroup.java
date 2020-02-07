5c36da4aae3e5e4eb6194a98fc628c8f4707551a
chanin <pci2676@gmail.com>
2020-02-08 00:27:21 +0900

    refactor 피드백 반영
     OperatorGroup, RawEquationSplitter - 상수로 분리할 수 있는 메세지 분리
     RawEquationSplitter - 조건문의 조건절을 명시적으로 알아 볼 수 있도록 메서드로 분리

--- src/main/java/calculator/domain/operator/OperatorGroup.java
+++ src/main/java/calculator/domain/operator/OperatorGroup.java
@@ -9,6 +9,8 @@ public enum OperatorGroup {
     MULTIPLE("*", (a, b) -> a * b),
     DIVIDE("/", (a, b) -> a / b);
 
+    private static final String NOT_FOUND_OPERATOR_MESSAGE = "%s는 등록되지 않은 연산자 입니다.";
+
     private final OperationStrategy operationStrategy;
     private final String delimiter;
 
@@ -21,7 +23,7 @@ public enum OperatorGroup {
         return Arrays.stream(values())
                 .filter(aOperator -> aOperator.isEqual(delimiter))
                 .findFirst()
-                .orElseThrow(() -> new NoSuchElementException(delimiter + "는 등록되지 않은 연산자 입니다."));
+                .orElseThrow(() -> new NoSuchElementException(String.format(NOT_FOUND_OPERATOR_MESSAGE, delimiter)));
     }
 
     public double operate(double a, double b) {
--- src/main/java/calculator/util/RawEquationSplitter.java
+++ src/main/java/calculator/util/RawEquationSplitter.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class RawEquationSplitter {
     private static final String BLANK = " ";
     private static final String NUMBER_FORMAT = "\\d+";
+    private static final String INCORRECT_POSITION_MESSAGE = "식이 올바르지 않습니다. %s의 위치를 확인하세요.";
+    private static final String INCORRECT_POSITION_NUMBER = "숫자";
+    private static final String INCORRECT_POSITION_OPERATOR = "연산자";
 
     public static List<String> split(String inputString) {
         String[] result = inputString.split(BLANK);
@@ -20,12 +23,20 @@ public class RawEquationSplitter {
     }
 
     private static void checkPosition(String[] equation, int index) {
-        if (index % 2 != 0 && isNumber(equation[index])) {
-            throw new IllegalArgumentException("식이 올바르지 않습니다. 숫자의 위치를 확인하세요.");
+        if (isNotNumberPosition(index) && isNumber(equation[index])) {
+            throw new IllegalArgumentException(String.format(INCORRECT_POSITION_MESSAGE, INCORRECT_POSITION_NUMBER));
         }
-        if (index % 2 == 0 && isNotNumber(equation[index])) {
-            throw new IllegalArgumentException("식이 올바르지 않습니다. 연산자의 위치를 확인하세요.");
+        if (isNumberPosition(index) && isNotNumber(equation[index])) {
+            throw new IllegalArgumentException(String.format(INCORRECT_POSITION_MESSAGE, INCORRECT_POSITION_OPERATOR));
         }
+    }
+
+    private static boolean isNotNumberPosition(int index) {
+        return index % 2 != 0;
+    }
+
+    private static boolean isNumberPosition(int index) {
+        return !isNotNumberPosition(index);
     }
 
     private static boolean isNotNumber(String maybeNumber) {
