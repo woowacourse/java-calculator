6294177812e8bb5d3812c4afc0d6f348bcfb4220
lalize <printf@kakao.com>
2020-02-07 16:35:21 +0900

    refactor: Calculator 클래스 calculate 메소드 리팩토링

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -1,16 +1,11 @@
 package study;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Calculator {
 	public static double calculate(String[] values) {
-		Queue<String> elements = new LinkedList<>(Arrays.asList(values));
-		double result = Double.parseDouble(elements.poll());
-		while (!elements.isEmpty()) {
-			String operator = elements.poll();
-			double rightOperand = Double.parseDouble(elements.poll());
+		double result = Double.parseDouble(values[0]);
+		for (int i = 1; i < values.length; i += 2) {
+			String operator = values[i];
+			double rightOperand = Double.parseDouble(values[i + 1]);
 			result = Operator.of(operator)
 					.calculate(result, rightOperand);
 		}
