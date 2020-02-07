c61810b1b649d6aa489a8ca41b142358e5f92b4f
jaeju.jang <jjj0611@gmail.com>
2020-02-07 16:23:02 +0900

    add. Application 클래스에서 사용자에게 식을 입력 받아 결과를 출력(기능 4 & 5)

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,18 @@
-public class Application {
-    public static void main(String[] args) {
+import expression.Expression;
 
+import java.util.Scanner;
+
+public class Application {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("수식을 입력해주세요.(공백으로 구분)");
+        String userExpression = scanner.nextLine();
+
+        Expression expression = new Expression(userExpression);
+        Double userResult = expression.getResult();
+
+        System.out.println("결과 : " + userResult);
     }
 }
