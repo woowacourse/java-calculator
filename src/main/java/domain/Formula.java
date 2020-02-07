f8878f7df5c00b59ce7c7934f609d6af0f71181c
JunSeong <ebseud6135@naver.com>
2020-02-08 01:08:11 +0900

    검증된 식만을 저장할 Formula 클래스 추가 및 Formula를 매개변수로 분리된 식을 저장할 Formulas 수정

---
+++ src/main/java/domain/Formula.java
@@ -0,0 +1,43 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Formula {
+    private static final String BLANK = " ";
+
+    private String formula;
+
+    public Formula(String formula) {
+        checkFormula(formula.split(BLANK));
+        this.formula = formula;
+    }
+
+    private void checkFormula(String[] formulas) {
+        validNumbers(formulas[0]);
+        for (int index = 1; index < formulas.length; index += 2) {
+            validOperator(formulas[index]);
+            validNumbers(formulas[index + 1]);
+        }
+    }
+
+    private void validNumbers(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("피연산자가 숫자가 아닙니다. " + number);
+        }
+    }
+
+    private void validOperator(String operator) {
+        char first = operator.charAt(0);
+        if (!(first == '*' || first == '/' || first == '+' || first == '-') || operator.length() != 1) {
+            throw new IllegalArgumentException("연산자가 잘못되었습니다. " + operator);
+        }
+    }
+
+    public List<String> toList() {
+        return Arrays.asList(formula.split(BLANK));
+    }
+
+}
--- src/main/java/domain/Formulas.java
+++ src/main/java/domain/Formulas.java
@@ -6,17 +6,20 @@ import java.util.List;
 
 public class Formulas {
     private final List<String> formulas;
-    private static final String BLANK = " ";
 
-    public Formulas(String formula) {
-        this.formulas = Arrays.asList(formula.split(BLANK));
+    public Formulas(Formula formula) {
+        this.formulas = formula.toList();
     }
 
     public int size() {
         return formulas.size();
     }
 
-    public String get(int index) {
+    public String getOperation(int index) {
         return formulas.get(index);
+    }
+
+    public double getNumToDouble(int index) {
+        return Double.parseDouble(formulas.get(index));
     }
 }
