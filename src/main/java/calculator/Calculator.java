1b82e41b0ea572bad4375766201e0718c8d1d64e
jaeju.jang <jjj0611@gmail.com>
2020-02-07 17:23:46 +0900

    add. CalculatorPrinter 클래스에 안내문 출력 기능 추가.
    	- Calculator를 시작하면 안내문 출력

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,13 +5,12 @@ import input.CalculatorInputScanner;
 import output.CalculatorPrinter;
 
 public class Calculator {
-
     public void run() {
+        CalculatorPrinter.printIntroduction();
         while (true) {
             String userExpressionInput = CalculatorInputScanner.getExpression();
             Expression expression = new Expression(userExpressionInput);
             CalculatorPrinter.print(expression);
         }
     }
-
 }
