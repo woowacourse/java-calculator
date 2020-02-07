956cbf9d515ceb69437363fe6994db77b4c70aa2
lalize <printf@kakao.com>
2020-02-07 17:45:33 +0900

    test: calculate 테스트 케이스 추가

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -9,7 +9,7 @@ import domain.Calculator;
 
 public class CalculatorTest {
 	@ParameterizedTest
-	@CsvSource(value = {"2 + 3 * 4 / 2,10", "2 + 4 / 2 * 6,18"})
+	@CsvSource(value = {"2 + 3 * 4 / 2,10", "2 + 4 / 2 * 6,18", "-2 + 3 * 4 + -12,-8"})
 	void calculate(String expression, int expected) {
 		String[] values = expression.split(" ");
 		assertThat(Calculator.calculate(values)).isEqualTo(expected);
