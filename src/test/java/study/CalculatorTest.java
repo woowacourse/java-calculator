15767290c8dd70c228e11436be0f9917f86682a4
aegis1920 <aegis1920@gmail.com>
2020-02-07 17:15:32 +0900

    mod : @Displayname 추가

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -14,20 +15,6 @@ public class CalculatorTest {
     private static final int ODD = 1;
     static double returnValue;
     static String nowSign;
-
-    @Test
-    public static void main(String args[]) {
-        String[] values = ExceptionHandlerTest.inputHandler().split(" ");
-        returnValue = Double.parseDouble(values[0]);
-        for (int i = INDEX_INIT; i < values.length; i++) {
-            check(i, values[i]);
-        }
-        if (returnValue == (int) returnValue) {
-            System.out.println((int) returnValue);
-            return;
-        }
-        System.out.println(returnValue);
-    }
 
     @BeforeEach
     void setUp() {
--- src/test/java/study/ExceptionHandlerTest.java
+++ src/test/java/study/ExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -34,6 +35,7 @@ public class ExceptionHandlerTest {
     }
 
     @Test
+    @DisplayName("입력값을 체크해주는 테스트")
     public void checkInputHandlerTest() {
         String str = "321 + 3 + f";
         assertThatThrownBy(() -> {
@@ -57,6 +59,7 @@ public class ExceptionHandlerTest {
     }
 
     @Test
+    @DisplayName("문자열을 split하고 전체적으로 체크해주는 테스트")
     void checkStringTest() {
         String[] inputStrings = "333 + 2434343".split(" ");
         assertThat(checkString(inputStrings)).isTrue();
@@ -77,8 +80,8 @@ public class ExceptionHandlerTest {
     }
 
     @Test
+    @DisplayName("숫자인지 체크해주는 테스트")
     void checkNumberTest() {
-        // return이 boolean이라 isTrue, isFalse로만 판별?
         String str = "g";
         assertThat(checkNumber(str)).isFalse();
     }
@@ -100,6 +103,7 @@ public class ExceptionHandlerTest {
     }
 
     @Test
+    @DisplayName("사칙연산인지 체크해주는 테스트")
     void checkSignTest() {
         String str = "@";
         assertThat(checkSign(str)).isFalse();
@@ -113,8 +117,9 @@ public class ExceptionHandlerTest {
     }
 
     @Test
+    @DisplayName("0으로 나누었을 때의 예외처리를 해주는 테스트")
     void isUndefinedValueTest() {
-        String str = "@";
-        assertThat(isUndefinedValue("0 / 0")).isFalse();
+        String str = "0 / 0";
+        assertThat(isUndefinedValue(str)).isFalse();
     }
 }
