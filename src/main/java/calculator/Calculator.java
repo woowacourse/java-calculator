1e7ddf44a6a1f6e2bdbcda66db032e4741efa10a
gunhee lee <dlrjsgml42@naver.com>
2020-02-07 17:25:58 +0900

    feat: 코드에 문서화 주석 작성 및 코드 리팩토링
    코드에 문서화 주석을 작성하였고, 추가로 코드 인덴트와 상수값의 이름 조정, 필요없는 변수 삭제를 수행하였습니다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,58 +2,105 @@ package calculator;
 
 import java.util.*;
 
+/**
+ * Calculator 클래스는 계산기의 기능을 수행하는 클래스로,
+ * 입력, 데이터 검증, 사칙연산 수행, 결과값 출력을 메서드로 가진다.
+ */
+
 public class Calculator {
-    private final static String plus = "+";
-    private final static String minus = "-";
-    private final static String multiply = "*";
-    private final static String divide = "/";
-    private final static String end = "q";
+    /**
+     * final static String 형식으로 plus, minus, multiply, divide를 선언하였다.
+     * 직접 상수값을 코드에 기재하지 않기 때문에 수정이 용이할 것이다.
+     * 그러나, 연산 메서드 자체까지 대체하고 더 높은 안정성을 가지도록 enum으로 대체가 가능하리라 생각된다.
+     * 시간이 난다면 꼭 수정해야 할 부분이다.
+     */
+    private final static String PLUS = "+";
+    private final static String MINUS = "-";
+    private final static String MULTIPLY = "*";
+    private final static String DIVIDE = "/";
 
-    public Calculator() {}
+    /**
+     * Calculator는 생성자 메서드이다. 수행하는 작업은 없다.
+     */
+    public Calculator() {
+    }
 
+    /**
+     * start 메서드는 계산기의 메인 동작을 수행한다.
+     * 올바르지 않은 입력이 들어오면 입력을 다시 요청하고,
+     * 올바른 값이 들어오면 값을 출력해준다.
+     * 이 때 while을 통한 루프 구조 대신,
+     * 자기 자신을 재귀적으로 호출하여 올바른 값이 나올 까지 실행하도록 설계하였다.
+     *
+     * @throws ArithmeticException    : 만약 0으로 나누기 오류가 발생하면 예외처리한다
+     * @throws NumberFormatException  : 만약 입력받은 숫자가 유효하지 않은 값이라면 예외처리한다
+     * @throws InputMismatchException : 만약 입력받은 수식 값이 유효하지 않은 값이라면 예외처한다
+     */
     public void start() {
-        Integer result = null;
-        while (true) {
-            System.out.println("\n계산할 수식을 입력해주세요. (숫자와 연산자는 공백으로 구분)\n ex. 1 + 2");
-            String inputEq = inputEquation();
-            if (!validateString(inputEq)) {
-                System.out.println("빈 문자열입니다. 다시 입력해주세요.");
-                continue;
-            }
-            Queue<String> equation = makeQueue(inputEq);
-            try {
-                result = execute(equation);
-            } catch (ArithmeticException e1) {
-                System.out.println("0으로 나눌 수 없습니다. 다시 입력해주세요.");
-                continue;
-            } catch (NumberFormatException e2) {
-                System.out.println("잘못된 수식이 입력되었습니다. 다시 입력해주세요.");
-                continue;
-            } catch (InputMismatchException e3) {
-                System.out.println("잘못된 수식이 입력되었습니다. 다시 입력해주세요.");
-                continue;
-            }
-            if (result != null) {
-                System.out.println(result);
-                break;
-            }
+        System.out.println("\n계산할 수식을 입력해주세요. (숫자와 연산자는 공백으로 구분)");
+        String inputString = inputEquation();
+        if (validateString(inputString)) {
+            System.out.println("빈 문자열입니다. 다시 입력해주세요.");
+            start();
+        }
+        Queue<String> equation = makeQueue(inputString);
+        try {
+            System.out.println(execute(equation));
+        } catch (ArithmeticException e1) {
+            System.out.println("0으로 나눌 수 없습니다. 다시 입력해주세요.");
+            start();
+        } catch (NumberFormatException e2) {
+            System.out.println("잘못된 수식이 입력되었습니다. 다시 입력해주세요.");
+            start();
+        } catch (InputMismatchException e3) {
+            System.out.println("잘못된 수식이 입력되었습니다. 다시 입력해주세요.");
+            start();
         }
     }
 
+    /**
+     * inputEquation은 수식을 입력받고 값을 리턴해준다.
+     *
+     * @return 입력받은 문자열 값을 반환한다.
+     */
     private String inputEquation() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
-    public boolean validateString(String inputEq) {
-        return inputEq != null && !inputEq.equals("");
+    /**
+     * validateString은 입력받은 문자열 값이 비어있는지(즉, 유효하지 않은 문자열인지) 확인하는 메서드이다.
+     *
+     * @param inputString 입력받은 문자열을 파라미터로 전달한다.
+     * @return 만약 문자열이 올바르지 않다면 true를, 올바르다면 false를 반환한다.
+     */
+    public boolean validateString(String inputString) {
+        return inputString == null || inputString.equals("");
     }
 
-    public Queue makeQueue(String inputString) {
+    /**
+     * makeQueue는 입력받은 문자열을 공백 기준으로 자르고,
+     * 그 값을 큐에 넣어 반환해주는 메서드이다.
+     *
+     * @param inputString 입력받은 문자열을 파라미터로 전달한다.
+     * @return 입력받은 값을 문자열 큐에 넣어 반환한다.
+     */
+    public Queue<String> makeQueue(String inputString) {
         String[] elements = inputString.split("\\s+", 0);
         return new LinkedList<>(Arrays.asList(elements));
     }
 
+    /**
+     * execute는 실제 메인 로직(각각의 숫자, 연산자를 큐에서 뽑아내어 계산하는 것)을 수행하는 메서드이다.
+     * 수행 과정에서 다양한 오류에 대응하여 예외처리를 throw 한다.
+     * 큐가 빌 경우 연산을 끝내고 지금까지 누적된 정수형 연산값을 반환해준다.
+     *
+     * @param equation 수식이 저장되어 있는 문자열 큐를 전달한다.
+     * @return 계산이 끝난 정수형 결과값을 반환한다.
+     * @throws ArithmeticException    만약 연산 과정에서 0으로 나누기 에러가 발생 시 throw한다.
+     * @throws NumberFormatException  만약 연산 과정에서 올바르지 않은 숫자값이 발견될 시 throw한다.
+     * @throws InputMismatchException 만약 연산 과정에서 올바르지 않은 문자열(연산자) 값이 발견될 시 throw한다.
+     */
     public int execute(Queue<String> equation) throws ArithmeticException, NumberFormatException, InputMismatchException {
         int result = validateNumber(equation.poll());
         while (!equation.isEmpty()) {
@@ -64,6 +111,14 @@ public class Calculator {
         return result;
     }
 
+    /**
+     * validateNumber는 숫자값이 유효한지 검사하는 메서드이다.
+     * 값을 변환하면서 만약 예외 발생시 이를 throw한다.
+     *
+     * @param number 검사할 String형 숫자를 파라미터로 전달받는다.
+     * @return 예외 미발생시 숫자값을 정수형으로 변환하여 반환한다.
+     * @throws NumberFormatException 만약 값이 유효하지 않은 숫자값(오버플로우, 숫자가 아닌 문자열 등)일 경우 throw한다.
+     */
     public int validateNumber(String number) {
         try {
             return Integer.parseInt(number);
@@ -72,33 +127,74 @@ public class Calculator {
         }
     }
 
+    /**
+     * calculate는 두 개의 정수 값과 연산자 값을 입력받고,
+     * 연산자 값에 따라 정해진 연산을 두 숫자에 수행하여 결과값을 반환한다.
+     * 만약 enum을 통해 PLUS,MINUS 등의 메서드가 등록되어 있다면 switch가 없어도 될듯하다.
+     * 이에 대해서는 추후 수정할 예정이다. (문서 주석 20.02.07 작성)
+     *
+     * @param result   이전에 계산해 둔 정수형 값을 전달받는다.
+     * @param operator 새로 수행할 문자열 연산을 전달받는다.
+     * @param operand  새로 연산을 수행할 정수형 숫자를 전달받는다.
+     * @return 연산에 따른 정수형 결과값을 반환한다.
+     * @throws InputMismatchException 만약 연산자 문자열이 사칙연산자 이외의 값이라면 예외를 발생시킨다.
+     * @throws ArithmeticException    만약 0으로 나누는 연산 발생 시 예외를 throw한다.
+     */
     public int calculate(int result, String operator, int operand) throws ArithmeticException {
         switch (operator) {
-            case plus:
+            case PLUS:
                 return add(result, operand);
-            case minus:
+            case MINUS:
                 return subtract(result, operand);
-            case multiply:
+            case MULTIPLY:
                 return multiply(result, operand);
-            case divide:
+            case DIVIDE:
                 return divide(result, operand);
             default:
                 throw new InputMismatchException("해당 연산자 없음");
         }
     }
 
+    /**
+     * add는 입력받은 두 정수형 값을 더하여 반환한다.
+     *
+     * @param a 계산을 수행할 정수형 숫자이다.
+     * @param b 계산을 수행할 정수형 숫자이다.
+     * @return 계산된 값을 반환한다.
+     */
     public int add(int a, int b) {
         return a + b;
     }
 
+    /**
+     * add는 입력받은 첫번째 정수값에서 두번째 정수값을 뺀 뒤 그 값을 반환한다.
+     *
+     * @param a 계산을 수행할 정수형 숫자이다.
+     * @param b 계산을 수행할 정수형 숫자이다.
+     * @return 계산된 값을 반환한다.
+     */
     public int subtract(int a, int b) {
         return a - b;
     }
 
+    /**
+     * add는 입력받은 두 정수형 값을 하여 반환한다.
+     *
+     * @param a 계산을 수행할 정수형 숫자이다.
+     * @param b 계산을 수행할 정수형 숫자이다.
+     * @return 계산된 값을 반환한다.
+     */
     public int multiply(int a, int b) {
         return a * b;
     }
 
+    /**
+     * divide는 입력받은 첫번째 값을 입력받은 두번째 값으로 나눠 그 값을 반환한다.
+     *
+     * @param a 계산을 수행할 정수형 숫자이다.
+     * @param b 계산을 수행할 정수형 숫자이다.
+     * @return 계산된 값을 반환한다.
+     */
     public int divide(int a, int b) {
         try {
             return a / b;
--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,7 +1,11 @@
 package calculator;
 
+/**
+ * Main 클래스는 main 메서드를 포함하는, 프로그램 실행 시 수행되는 클래스이다.
+ */
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
         calculator.start();
     }
