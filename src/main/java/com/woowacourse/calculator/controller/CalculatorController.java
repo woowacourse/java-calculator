55453a7a734c68eaa55618f737811cb15cc83f3e
toneyparky <qkrtmddhks95@gmail.com>
2020-02-07 13:44:00 +0900

    Add: CalculatorController class

---
+++ src/main/java/com/woowacourse/calculator/controller/CalculatorController.java
@@ -0,0 +1,35 @@
+package com.woowacourse.calculator.controller;
+
+import com.woowacourse.calculator.domain.Expression;
+import com.woowacourse.calculator.domain.ExpressionFactory;
+import com.woowacourse.calculator.view.InputView;
+import com.woowacourse.calculator.view.OutputView;
+
+/**
+ * 클래스 이름 : CalculatorController.java
+ *
+ * @author S.W.PARK
+ * @author K.S.KIM
+ * @version 1.0
+ * <p>
+ * 날짜 : 2020.02.06 목요일
+ */
+
+public class CalculatorController {
+
+    public void run() {
+        Expression expression = makeExpression();
+        double result = expression.calculate();
+        OutputView.printAnswer(result);
+    }
+
+    private Expression makeExpression() {
+        try {
+            String input = InputView.inputEquation();
+            return ExpressionFactory.create(input);
+        } catch (Exception e) {
+            OutputView.printRetryMessage();
+            return makeExpression();
+        }
+    }
+}
--- src/main/java/com/woowacourse/calculator/view/OutputView.java
+++ src/main/java/com/woowacourse/calculator/view/OutputView.java
@@ -12,11 +12,16 @@ package com.woowacourse.calculator.view;
 
 public class OutputView {
     private static final String RETRY_INPUT_NOTICE_MESSAGE = "잘못된 입력입니다. 다시 입력해주세요";
+    private static final String ANSWER_MESSAGE = "연산 결과는 %s 입니다.\n";
 
     // 묵시적 생성자 방지
     public OutputView() {}
 
     public static void printRetryMessage() {
         System.out.println(RETRY_INPUT_NOTICE_MESSAGE);
+    }
+
+    public static void printAnswer(double answer) {
+        System.out.printf(ANSWER_MESSAGE, answer);
     }
 }
