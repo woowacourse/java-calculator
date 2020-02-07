3354a1f59b0b919f39556db4eab3922b002c46b7
taejinseok <stj930@gmail.com>
2020-02-07 16:50:51 +0900

    [feat] main 함수 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,21 @@
+import domain.Calculator;
+import util.InputView;
+import util.OutputView;
+
+public class Application {
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		int result = getApplyResult(calculator);
+		OutputView.printResult(result);
+	}
+
+	private static int getApplyResult(Calculator calculator) {
+		try {
+			System.out.print("계산식을 입력해주세요 :");
+			return calculator.apply(InputView.input());
+		} catch (IllegalArgumentException ex) {
+			System.out.println(ex.getMessage());
+			return getApplyResult(calculator);
+		}
+	}
+}
