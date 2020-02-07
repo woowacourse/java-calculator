5108d2f2abe073b718b08fe09263bc405fb5da0c
Ramen6315 <ramen6315@gmail.com>
2020-02-07 17:19:26 +0900

    테스트 코드 작성
     - "/" 다음에 0이 오는 경우, 각연산에 따른 계산, 올바른계산이 되는지
       테스트 코드 작성
     -  코드 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -81,22 +81,22 @@ public class Calculator {
 
     private void checkOperatorValue(String[] equations) {
         for(int i=1; i<equations.length; i += 2) {
-            if(isWrongOperator(equations[i])) { throw new NumberFormatException(); }
+            if(isWrongOperator(equations, i)) { throw new NumberFormatException(); }
             if(isInvalidEquation(equations)) { throw new NumberFormatException(); }
             if(isDividedByZero(equations, i)) { throw new NumberFormatException(); }
         }
     }
 
-    public boolean isDividedByZero(String[] equations, int i) {
-        return equations[i].equals("/") && Double.parseDouble(equations[i+1]) == 0.0;
+    public boolean isWrongOperator(String[] equations, int index) {
+        return !equations[index].equals("+") && !equations[index].equals("-") && !equations[index].equals("*") && !equations[index].equals("/");
     }
 
     public boolean isInvalidEquation(String[] equations) {
         return equations.length % 2 == 0;
     }
 
-    public boolean isWrongOperator(String equation) {
-        return !equation.equals("+") && !equation.equals("-") && !equation.equals("*") && !equation.equals("/");
+    public boolean isDividedByZero(String[] equations, int index) {
+        return equations[index].equals("/") && Double.parseDouble(equations[index+1]) == 0.0;
     }
 
     public void checkNumberValue(String[] equations) {
