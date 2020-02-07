fe98384a139add53c87c8cebd2cccb729fe28ac7
Tak Hyungmin <milkho11@naver.com>
2020-02-07 16:44:41 +0900

    [기능] 결과값 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
         System.out.println("계산식을 입력해주세요.");
         System.out.println("입력된 계산식은 사칙 연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정됩니다.");
     }
+
+    public static void printCalculate(double result) {
+        System.out.println(result);
+    }
 }
