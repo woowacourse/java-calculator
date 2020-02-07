844849078992681eb83d1f57e939a2e2a3bc5a40
junk-sound <wom2277@naver.com>
2020-02-07 17:27:13 +0900

    test : Calculate add

---
+++ src/test/java/domain/CalculatorTest.java
@@ -0,0 +1,23 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    void calculateAdd() {
+        int first = 1;
+        int second = 2;
+        NumberSentence numberSentence = new NumberSentence(Arrays.asList(first, second), new LinkedList<>(Collections.singletonList("+")));
+
+        assertThat(calculator.calculate(numberSentence)).isEqualTo(first + second);
+    }
+}
