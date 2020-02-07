fb5daf00c64cd9a5afb61f0416bb66c02334ffb9
JunSeong <ebseud6135@naver.com>
2020-02-07 16:59:52 +0900

    문자열 계산기 - 계산기 실행 기능 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,26 +3,44 @@ import domain.Formulas;
 import view.InputView;
 import view.OutputView;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
-        calculate();
+        startCalculate();
     }
 
-    private static void calculate() {
+    private static void startCalculate() {
         try {
-            OutputView.output(calculate2());
-        } catch (Exception e) {
+            OutputView.printResult(calculate());
+        } catch (IllegalArgumentException e) {
             OutputView.exceptionMessage(e.getMessage());
-            calculate();
+            startCalculate();
         }
     }
 
-    private static double calculate2() {
+    private static int calculate() {
         Formulas formulas = new Formulas(InputView.input());
-        double result = Double.parseDouble(formulas.get(0));
+        double result = checkNumbers(formulas.get(0));
         for (int index = 1; index < formulas.size(); index += 2) {
-            result = Calculator.calculate(formulas.get(index), result, formulas.get(index + 1));
+            result = Calculator.calculate(checkOperator(formulas.get(index)),
+                    result, checkNumbers(formulas.get(index + 1)));
         }
-        return result;
+        return (int) result;
+    }
+
+    private static double checkNumbers(String number) {
+        if (!number.matches("^[0-9\\+\\-]?[0-9]+$")) {
+            throw new IllegalArgumentException("잘못된 계산식 - 피연산자가 숫자가 아님 : " + number);
+        }
+        if (Long.parseLong(number) > Integer.MAX_VALUE || Long.parseLong(number) < Integer.MIN_VALUE) {
+            throw new IllegalArgumentException("잘못된 계산식 - 피연산자가 INT 범위를 벗어남 : " + number);
+        }
+        return Double.parseDouble(number);
+    }
+
+    private static String checkOperator(String operator) {
+        if (operator.matches("^[\\+\\-\\*\\/]$")) {
+            return operator;
+        }
+        throw new IllegalArgumentException("잘못된 계산식 - 연산자가 잘못됨 : " + operator);
     }
 }
