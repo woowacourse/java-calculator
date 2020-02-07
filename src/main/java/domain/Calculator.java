03a86b206b71b7a0aa157ea5237d2f277d17adf6
JunSeong <ebseud6135@naver.com>
2020-02-08 01:11:47 +0900

    계산을 담당하는 Calculator클래스 수정
    나누기 계산을 할 때마다 0을 나누는지 검증하는 과정 추가
    static메소드들로 전부 수정

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -7,45 +7,32 @@ import java.util.function.BiFunction;
 public class Calculator {
     private static Map<String, BiFunction<Double, Double, Double>> operators = new HashMap<>();
 
-    private final Formulas formulas;
     static {
         operators.put("+", (num1, num2) -> num1 + num2);
         operators.put("-", (num1, num2) -> num1 - num2);
         operators.put("*", (num1, num2) -> num1 * num2);
-        operators.put("/", (num1, num2) -> num1 / num2);
+        operators.put("/", (num1, num2) -> {
+            validDivideZero(num2);
+            return num1 / num2;
+        });
     }
 
-    public Calculator(Formulas formulas) {
-        this.formulas = formulas;
-    }
-
-    public int startCalculate() {
-        double result = checkNumbers(formulas.get(0));
-        for (int index = 1; index < formulas.size(); index += 2) {
-            result = calculate(checkOperator(formulas.get(index)),
-                    result, checkNumbers(formulas.get(index + 1)));
-        }
-        return (int) result;
-    }
-
-    private double checkNumbers(String number) {
-        if (!number.matches("^[0-9\\+\\-]?[0-9]+$")) {
-            throw new IllegalArgumentException("잘못된 계산식 - 피연산자가 숫자가 아님 : " + number);
-        }
-        return Double.parseDouble(number);
-    }
-
-    private String checkOperator(String operator) {
-        if (operator.matches("^[\\+\\-\\*\\/]$")) {
-            return operator;
-        }
-        throw new IllegalArgumentException("잘못된 계산식 - 연산자가 잘못됨 : " + operator);
-    }
-
-    private double calculate(String operator, Double num1, Double num2) {
-        if (operator.equals("/") && num2.intValue() == 0) {
+    private static void validDivideZero(double num) {
+        if (num == 0) {
             throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
         }
+    }
+
+    public static int startCalculate(Formulas formulas) {
+        double accumulationValue = formulas.getNumToDouble(0);
+        for (int index = 1; index < formulas.size(); index += 2) {
+            accumulationValue = calculate(formulas.getOperation(index),
+                    accumulationValue, formulas.getNumToDouble(index + 1));
+        }
+        return (int) accumulationValue;
+    }
+
+    private static double calculate(String operator, Double num1, Double num2) {
         return operators.get(operator).apply(num1, num2);
     }
 }
