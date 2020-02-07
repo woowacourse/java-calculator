addb5c7597177c32b8f8e6a44b296e39815dfc4f
hamliet <hamliet@hanyang.ac.kr>
2020-02-07 13:30:46 +0900

    [add] 문자열 계산기 - 홀수 인덱스와 짝수 인덱스로 나누는 함수

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class Calculator {
     static int returnValue;
     private static final int INDEX_INIT = 1;
+    private static final int EVEN = 0;
+    private static final int ODD = 1;
     private static Scanner scanner = new Scanner(System.in);
+    private static String nowSign;
 
     public static void main(String args[]) {
         String[] values = scanner.nextLine().split(" ");
@@ -12,5 +15,14 @@ public class Calculator {
             //check 함수
         }
         System.out.println(returnValue);
+    }
+
+    private static void check(int i, String value) {
+        if (i % 2 == EVEN) {
+            //계산하는 함수
+        }
+        if (i % 2 == ODD) {
+            nowSign = value;
+        }
     }
 }
