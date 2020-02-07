0551dc48bf78eff2238eb2a758e93015c8b6c5ca
MOON JINJU <mjj920930@gmail.com>
2020-02-07 17:38:43 +0900

    feat: 위의 세가지를 검증 후 예외인 경우 다시 입력받는 기능 구현

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -2,12 +2,17 @@ package calculator;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            String[] inputs = StringHandler.getInputAndSplit();
-            Source source = new Source(inputs);
-            source.printResult(source.calculateInputs());
-        } catch (Exception e) {
-            e.printStackTrace();
+        while (true) {
+            try {
+                System.out.println("계산할 식을 입력해주세요.");
+                String[] inputs = StringHandler.getInputAndSplit();
+                Source source = new Source(inputs);
+                source.printResult(source.calculateInputs());
+                return;
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
         }
     }
 }
