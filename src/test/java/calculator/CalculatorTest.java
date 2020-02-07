0841c0b8fa1696c4162dc79047e9278dfd8b90b9
JunSeong <ebseud6135@naver.com>
2020-02-07 17:41:39 +0900

    문자열 계산기 - Calculator클래스 단위 테스트 수정

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,4 +1,22 @@
 package calculator;
 
+import domain.Calculator;
+import domain.Formulas;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalculatorTest {
+
+    @Test
+    void calculateTest() {
+        Calculator calculator = new Calculator(new Formulas("1 + 2"));
+        int result = calculator.startCalculate();
+        assertThat(result).isEqualTo(3);
+        Calculator calculator2 = new Calculator(new Formulas("2 + 3 * 4 / 2"));
+        result = calculator2.startCalculate();
+        assertThat(result).isEqualTo(10);
+    }
+
+
 }
