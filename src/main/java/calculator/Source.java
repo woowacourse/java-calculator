cf06572aa664db98de4d77a83d5789908b7827e7
MOON JINJU <mjj920930@gmail.com>
2020-02-07 16:08:03 +0900

    fix: 인덱스에 따라 검증한 후 플래그를 변경하는 기능 추가

--- src/main/java/calculator/Source.java
+++ src/main/java/calculator/Source.java
@@ -45,5 +45,6 @@ public class Source {
             CalculatorType operator = CalculatorType.validateOperator(input);
             operators.add(operator);
         }
+        operandTurn = !operandTurn;
     }
 }
