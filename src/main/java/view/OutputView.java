f44846b3b130a5e6d1b521922ceda69cf6f0cbc6
LTTTTTE <10100aaaaa@gmail.com>
2020-02-07 17:15:56 +0900

    feat : Main 클래스 생성 및 기능 적용

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,11 +4,11 @@ public class OutputView {
 	private static final String RESULT = "계산 결과: ";
 	private static final String RETRY = "에러: 다시 입력하세요!";
 
-	public void showResult(double result) {
+	public static void showResult(double result) {
 		System.out.println(RESULT + result);
 	}
 
-	public void showRetry() {
+	public static void showRetry() {
 		System.out.println(RETRY);
 	}
 }
