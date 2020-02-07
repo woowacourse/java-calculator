625961299cacb3ba13820368ec617f5ba4a114eb
woonjangahn <woonjangahn@gmail.com>
2020-02-07 10:30:51 +0900

    FEAT: 입력 예외처리 및 테스트 추가

--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -1,13 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputValidator {
 
 	public boolean validate(String input) {
 		if (!validateWithRegex(input)) {
 			return true;
 		}
+		String[] splittedInput = input.split(" ");
+		if (validateAfterSplit(splittedInput)) {
+			return true;
+		}
 		return false;
 	}
 
 	private boolean validateWithRegex(String input) {
-		return input.matches("[0-9+-/*.\\s]+");
+		return input.matches("[0-9+*/\\-.\\s]+[0-9]+$");
+	}
+
+	private boolean validateAfterSplit(String[] input) {
+		List<String> numbers = new ArrayList<>();
+		List<String> operators = new ArrayList<>();
+		for (int i = 0; i < input.length; i++) {
+			if (i % 2 == 0) {
+				numbers.add(input[i]);
+			} else {
+				operators.add(input[i]);
+			}
+		}
+		for (String number : numbers) {
+			try {
+				Double.parseDouble(number);
+			} catch (Exception e) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- src/test/java/InputValidatorTest.java
+++ src/test/java/InputValidatorTest.java
@@ -6,7 +6,37 @@ public class InputValidatorTest {
 	private InputValidator inputValidator = new InputValidator();
 
 	@Test
+	void returnFalseIfInputIsValid() {
+		assertThat(inputValidator.validate("1 + 2 + 3 + 4.0")).isFalse();
+	}
+
+	@Test
 	void returnTrueIfInputIncludesInvalidCharacters() {
 		assertThat(inputValidator.validate("1 + 2 + 3 + 4.0 + ^")).isTrue();
+	}
+
+	@Test
+	void returnTrueIfInputEndsWithOperator() {
+		assertThat(inputValidator.validate("1 + 2 + 3 + 4.0 +")).isTrue();
+	}
+
+	@Test
+	void returnTrueIfInputStartsWithOperator() {
+		assertThat(inputValidator.validate("+ 1 - 5 / 6")).isTrue();
+	}
+
+	@Test
+	void returnTrueIfSplittedInputStartsWithInvalidOperator() {
+		assertThat(inputValidator.validate("1 + *3 + /2 + 3 + 3")).isTrue();
+	}
+
+	@Test
+	void returnTrueIfSplittedInputElementContainsNumberAndOperator() {
+		assertThat(inputValidator.validate("1+2 + 123")).isTrue();
+	}
+
+	@Test
+	void returnTrueIfNumberOrOperatorRepeats() {
+		assertThat(inputValidator.validate("1 1 - 2 + + 3"));
 	}
 }
