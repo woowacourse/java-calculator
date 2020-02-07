4b1a800972f8ab98c48920c834e6ed258f5f0d45
moonyoungCHAE <xpf_fl@naver.com>
2020-02-07 16:31:31 +0900

    문자열 계산기  테스트 코드 수정
    프로덕션 코드를 수정하면서 테스트 코드도 수정함

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,13 +5,14 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalculatorTest {
     @ParameterizedTest
-    @DisplayName("실수형 검증")
-    @CsvSource(value = {"3:true", "1.9:true", "문자 입력:false",
-            "+:false", "-1:true"}, delimiter = ':')
+    @DisplayName("올바른 숫자를 입력하였는지 검증")
+    @CsvSource(value = {"3 + ! + 4:false", "1.9 * 6.2:true", "+ + 4:false",
+            "5 * 6 * 7 * 9:true", "0 / 0:true"}, delimiter = ':')
     void validateDoubleTest(String input, boolean expected) {
         boolean actualResult = true;
         try {
-            Validator.validateDouble(input);
+            InputValues inputValues = new InputValues(input);
+            inputValues.validateDouble();
         } catch (Exception e) {
             actualResult = false;
         }
@@ -19,13 +20,14 @@ public class CalculatorTest {
     }
 
     @ParameterizedTest
-    @DisplayName("연산자 검증")
-    @CsvSource(value = {"3:false", "+:true", "/:true",
-            "문자입력:false", "):false"}, delimiter = ':')
+    @DisplayName("올바른 연산자를 입력하였는지 검증")
+    @CsvSource(value = {"2 a 3:false", "2 * 3 + 4 +:true", "2 ! 3:false",
+            "2 2 2:false", ") ):false"}, delimiter = ':')
     void validateOperatorTest(String input, boolean expected) {
         boolean actualResult = true;
         try {
-            Validator.validateOperator(input);
+            InputValues inputValues = new InputValues(input);
+            inputValues.validateOperator();
         } catch (Exception e) {
             actualResult = false;
         }
@@ -33,10 +35,24 @@ public class CalculatorTest {
     }
 
     @ParameterizedTest
-    @DisplayName("연산자 enum 검증")
+    @DisplayName("연산자로 수식이 끝나는 경우 검증")
+    @CsvSource(value = {"2 + 3 * 5 / 2:true", "2 +:false", "2 + 3 *:false"}, delimiter = ':')
+    void validateEndWithOperatorTest(String input, boolean expected) {
+        boolean actualResult = true;
+        try {
+            InputValues inputValues = new InputValues(input);
+            inputValues.validateEndWithOperator();
+        } catch (Exception e) {
+            actualResult = false;
+        }
+        Assertions.assertThat(actualResult).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("연산자 enum이 올바른지 검증")
     @CsvSource(value = {"3:false", "+:true", "/:true",
             "문자입력:false", "):false"}, delimiter = ':')
-    void getOperatorByStringTest(String input, boolean expected){
+    void getOperatorByStringTest(String input, boolean expected) {
         boolean actualResult = true;
         try {
             Operator.getOperatorByString(input);
@@ -47,9 +63,9 @@ public class CalculatorTest {
     }
 
     @ParameterizedTest
-    @DisplayName("연산 결과 검증")
-    @CsvSource(value = {"+:1:2:3", "-:2:1:1", "/:1:0:0", "*:2:3:6"}, delimiter =':')
-    void operateTest(String stringOperator, double num1, double num2, double expected){
+    @DisplayName("연산 결과가 올바른지 검증")
+    @CsvSource(value = {"+:1:2:3", "-:2:1:1", "*:7:4:28", "*:2:3:6"}, delimiter = ':')
+    void operateTest(String stringOperator, double num1, double num2, double expected) {
         Operator operator = Operator.getOperatorByString(stringOperator);
         double actualResult = operator.operate(num1, num2);
 
