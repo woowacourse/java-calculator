68c0f94a63c7cc0bfd08e380424ab372ec350d78
JunSeong <ebseud6135@naver.com>
2020-02-07 17:40:20 +0900

    문자열 계산기 - 계산하는 기능 수정
    Main에서 담당하던 계산 로직을 Calculator에 몰아 넣는 방식으로 수정

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -7,6 +7,7 @@ import java.util.function.BiFunction;
 public class Calculator {
     private static Map<String, BiFunction<Double, Double, Double>> operators = new HashMap<>();
 
+    private final Formulas formulas;
     static {
         operators.put("+", (num1, num2) -> num1 + num2);
         operators.put("-", (num1, num2) -> num1 - num2);
@@ -14,7 +15,34 @@ public class Calculator {
         operators.put("/", (num1, num2) -> num1 / num2);
     }
 
-    public static double calculate(String operator, Double num1, Double num2) {
+    public Calculator(Formulas formulas) {
+        this.formulas = formulas;
+    }
+
+    public int startCalculate() {
+        double result = checkNumbers(formulas.get(0));
+        for (int index = 1; index < formulas.size(); index += 2) {
+            result = calculate(checkOperator(formulas.get(index)),
+                    result, checkNumbers(formulas.get(index + 1)));
+        }
+        return (int) result;
+    }
+
+    private static double checkNumbers(String number) {
+        if (!number.matches("^[0-9\\+\\-]?[0-9]+$")) {
+            throw new IllegalArgumentException("잘못된 계산식 - 피연산자가 숫자가 아님 : " + number);
+        }
+        return Double.parseDouble(number);
+    }
+
+    private static String checkOperator(String operator) {
+        if (operator.matches("^[\\+\\-\\*\\/]$")) {
+            return operator;
+        }
+        throw new IllegalArgumentException("잘못된 계산식 - 연산자가 잘못됨 : " + operator);
+    }
+
+    private static double calculate(String operator, Double num1, Double num2) {
         if (operator.equals("/") && num2.intValue() == 0) {
             throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
         }
