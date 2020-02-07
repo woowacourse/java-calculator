f7cc431a4407f60e9591eab0bd5d9cf69dd6f769
lalize <printf@kakao.com>
2020-02-08 00:28:16 +0900

    test: 0으로 나눌 수 없는 예외 발생 테스트

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -2,6 +2,7 @@ package study;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -13,5 +14,13 @@ public class CalculatorTest {
 	void calculate(String expression, double expected) {
 		String[] values = expression.split(" ");
 		assertThat(Calculator.calculate(values)).isEqualTo(expected);
+	}
+
+	@Test
+	void testZeroDivide() {
+		String[] values = "1 / 0".split(" ");
+		assertThatExceptionOfType(ArithmeticException.class).isThrownBy(() -> {
+			Calculator.calculate(values);
+		}).withMessage("0으로 나눌 수 없습니다.");
 	}
 }
