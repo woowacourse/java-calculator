a9971c21f901bd23d29b60010c4b2257ce6d1aff
lxxjn0 <ljy3510@gmail.com>
2020-02-08 17:34:45 +0900

    refactor : 실행을 위한 Application 클래스와 Calculator 객체 수정
    1. 정상적인 실행을 위한 Application 클래스 수정.
    2. Calculator 객체의 불필요한 맴버 변수 제거.

--- src/main/java/com/woowacourse/calculator/Application.java
+++ src/main/java/com/woowacourse/calculator/Application.java
@@ -2,11 +2,13 @@ package com.woowacourse.calculator;
 
 import com.woowacourse.calculator.controller.Calculator;
 import com.woowacourse.calculator.view.InputView;
+import com.woowacourse.calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
         final int calculateResult = calculator.calculate(InputView.inputExpression());
+		OutputView.printCalculateResult(calculateResult);
     }
 }
--- src/main/java/com/woowacourse/calculator/controller/Calculator.java
+++ src/main/java/com/woowacourse/calculator/controller/Calculator.java
@@ -4,10 +4,8 @@ import com.woowacourse.calculator.domain.Expression;
 import com.woowacourse.calculator.domain.Tokenizer;
 
 public class Calculator {
-	private Expression expression;
-
-    public int calculate(final String inputExpression) {
-		expression = new Expression(Tokenizer.tokenize(inputExpression));
+	public int calculate(final String inputExpression) {
+		Expression expression = new Expression(Tokenizer.tokenize(inputExpression));
 		return expression.calculate();
     }
 }
