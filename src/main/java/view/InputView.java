a08d1cb5c871c39c508ec8b571e96e2d3215c1c1
taejinseok <stj930@gmail.com>
2020-02-09 01:25:52 +0900

    [refactor] InputView, OutputView 입출력 메세지 상수로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,10 +3,11 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-	private static Scanner SCANNER = new Scanner(System.in);
+	private static final Scanner SCANNER = new Scanner(System.in);
+	private static final String EXPRESSION_INPUT_MESSAGE = "계산식을 입력해주세요: ";
 
 	public static String inputExpression() {
-		System.out.print("계산식을 입력해주세요 :");
+		System.out.print(EXPRESSION_INPUT_MESSAGE);
 		return SCANNER.nextLine();
 	}
 }
