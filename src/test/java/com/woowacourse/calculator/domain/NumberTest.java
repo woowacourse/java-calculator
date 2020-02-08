373121703d60b6e73c449c1a447f72c82f82aa82
lxxjn0 <ljy3510@gmail.com>
2020-02-08 18:03:46 +0900

    test : Number 객체 테스트 수정
    1. NumberTest 클래스 DisplayName 추가.

--- src/test/java/com/woowacourse/calculator/domain/NumberTest.java
+++ src/test/java/com/woowacourse/calculator/domain/NumberTest.java
@@ -2,15 +2,17 @@ package com.woowacourse.calculator.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Number Test")
 class NumberTest {
-	@Test
-	void testNumber() {
-		final String overflowNumber = "2147483648";
-		assertThatThrownBy(() -> {
-			new Number(overflowNumber);
-		}).isInstanceOf(NumberFormatException.class)
-			.hasMessage("유효한 범위의 숫자를 입력해 주세요");
-	}
+    @DisplayName("Number - Number() Test")
+    @Test
+    void testNumber() {
+        final String integerOverflowNumber = "2147483648";
+        assertThatThrownBy(() -> new Number(integerOverflowNumber))
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessage("유효한 범위의 숫자를 입력해 주세요");
+    }
 }
