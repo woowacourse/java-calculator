b2ffce3152331d565ffee4e3d8ea05bdc2709bbd
gunhee lee <dlrjsgml42@naver.com>
2020-02-08 18:27:35 +0900

    refactor: delete operator methods and use enum. also fix method accese and exceptions.
    연산자 메서드와 상수 값을 제거하고, enum을 통해 제어하도록 수정하였다.
    또한, 기존에 public으로 되어 있었던 접근 제어를 private로 수정하였다.
    더하여, 예외처리를 수행하는 부분을 묶어서 처리하도록 수정하였다.
    기능구역별로 commit하지 않고 몰아서 하게 되었다. 이에 대해서는 이후 분리할 수 있도록 노력할 것이다.
    추후 주석과 일부 문자열 값의 상수화에 대한 refactor 예정이다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,17 +9,6 @@ import java.util.*;
 
 public class Calculator {
     /**
-     * final static String 형식으로 plus, minus, multiply, divide를 선언하였다.
-     * 직접 상수값을 코드에 기재하지 않기 때문에 수정이 용이할 것이다.
-     * 그러나, 연산 메서드 자체까지 대체하고 더 높은 안정성을 가지도록 enum으로 대체가 가능하리라 생각된다.
-     * 시간이 난다면 꼭 수정해야 할 부분이다.
-     */
-    private final static String PLUS = "+";
-    private final static String MINUS = "-";
-    private final static String MULTIPLY = "*";
-    private final static String DIVIDE = "/";
-
-    /**
      * Calculator는 생성자 메서드이다. 수행하는 작업은 없다.
      */
     public Calculator() {
@@ -32,13 +21,18 @@ public class Calculator {
      * 이 때 while을 통한 루프 구조 대신,
      * 자기 자신을 재귀적으로 호출하여 올바른 값이 나올 까지 실행하도록 설계하였다.
      *
-     * @throws ArithmeticException    : 만약 0으로 나누기 오류가 발생하면 예외처리한다
-     * @throws NumberFormatException  : 만약 입력받은 숫자가 유효하지 않은 값이라면 예외처리한다
-     * @throws InputMismatchException : 만약 입력받은 수식 값이 유효하지 않은 값이라면 예외처한다
+     * Exception의 경우, IllegalArgumentException(enum 접근시 잘못된 값이 들어가는 경우),
+     * NumberFormatException(잘못된 숫자가 들어가는 경우, IllegalArgumentException의 하위 클래스라 기재하지 않음),
+     * InputMismatchException(잘못된 연산자 문자열이 들어가는 경우) 을 묶어서 처리한다.
+     * 세 경우 모두 유효하지 않은 문자열이고, 별도로 나누는 것이 의미없다고 생각하였기때문이다.
+     *
+     * @exception  ArithmeticException    : 만약 0으로 나누기 오류가 발생하면 예외처리한다
+     * @exception  IllegalArgumentException  : 만약 입력받은 숫자가 유효하지 않은 값이라면 예외처리한다
      */
     public void start() {
-        System.out.println("\n계산할 수식을 입력해주세요. (숫자와 연산자는 공백으로 구분)");
-        String inputString = inputEquation();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("계산할 수식을 입력해주세요. (숫자와 연산자는 공백으로 구분)");
+        String inputString = inputEquation(scanner);
         if (validateString(inputString)) {
             System.out.println("빈 문자열입니다. 다시 입력해주세요.");
             start();
@@ -46,13 +40,10 @@ public class Calculator {
         Queue<String> equation = makeQueue(inputString);
         try {
             System.out.println(execute(equation));
-        } catch (ArithmeticException e1) {
+        } catch (ArithmeticException arithmeticException) {
             System.out.println("0으로 나눌 수 없습니다. 다시 입력해주세요.");
             start();
-        } catch (NumberFormatException e2) {
-            System.out.println("잘못된 수식이 입력되었습니다. 다시 입력해주세요.");
-            start();
-        } catch (InputMismatchException e3) {
+        } catch (IllegalArgumentException | InputMismatchException e) {
             System.out.println("잘못된 수식이 입력되었습니다. 다시 입력해주세요.");
             start();
         }
@@ -63,8 +54,7 @@ public class Calculator {
      *
      * @return 입력받은 문자열 값을 반환한다.
      */
-    private String inputEquation() {
-        Scanner scanner = new Scanner(System.in);
+    private String inputEquation(Scanner scanner) {
         return scanner.nextLine();
     }
 
@@ -74,8 +64,8 @@ public class Calculator {
      * @param inputString 입력받은 문자열을 파라미터로 전달한다.
      * @return 만약 문자열이 올바르지 않다면 true를, 올바르다면 false를 반환한다.
      */
-    public boolean validateString(String inputString) {
-        return inputString == null || inputString.equals("");
+    private boolean validateString(String inputString) {
+        return "".equals(inputString);
     }
 
     /**
@@ -85,7 +75,7 @@ public class Calculator {
      * @param inputString 입력받은 문자열을 파라미터로 전달한다.
      * @return 입력받은 값을 문자열 큐에 넣어 반환한다.
      */
-    public Queue<String> makeQueue(String inputString) {
+    private Queue<String> makeQueue(String inputString) {
         String[] elements = inputString.split("\\s+", 0);
         return new LinkedList<>(Arrays.asList(elements));
     }
@@ -101,7 +91,7 @@ public class Calculator {
      * @throws NumberFormatException  만약 연산 과정에서 올바르지 않은 숫자값이 발견될 시 throw한다.
      * @throws InputMismatchException 만약 연산 과정에서 올바르지 않은 문자열(연산자) 값이 발견될 시 throw한다.
      */
-    public int execute(Queue<String> equation) throws ArithmeticException, NumberFormatException, InputMismatchException {
+    private int execute(Queue<String> equation) throws ArithmeticException, NumberFormatException, InputMismatchException, IllegalArgumentException {
         int result = validateNumber(equation.poll());
         while (!equation.isEmpty()) {
             String operator = equation.poll();
@@ -119,7 +109,7 @@ public class Calculator {
      * @return 예외 미발생시 숫자값을 정수형으로 변환하여 반환한다.
      * @throws NumberFormatException 만약 값이 유효하지 않은 숫자값(오버플로우, 숫자가 아닌 문자열 등)일 경우 throw한다.
      */
-    public int validateNumber(String number) {
+    private int validateNumber(String number) {
         try {
             return Integer.parseInt(number);
         } catch (NumberFormatException e) {
@@ -137,69 +127,10 @@ public class Calculator {
      * @param operator 새로 수행할 문자열 연산을 전달받는다.
      * @param operand  새로 연산을 수행할 정수형 숫자를 전달받는다.
      * @return 연산에 따른 정수형 결과값을 반환한다.
-     * @throws InputMismatchException 만약 연산자 문자열이 사칙연산자 이외의 값이라면 예외를 발생시킨다.
-     * @throws ArithmeticException    만약 0으로 나누는 연산 발생 시 예외를 throw한다.
+     * @throws IllegalArgumentException 만약 연산자 문자열이 사칙연산자 이외의 값이라면 enum 매칭 중 예외를 발생시킨다.
+     * @throws ArithmeticException      만약 0으로 나누는 연산 발생 시 예외를 throw한다.
      */
-    public int calculate(int result, String operator, int operand) throws ArithmeticException {
-        switch (operator) {
-            case PLUS:
-                return add(result, operand);
-            case MINUS:
-                return subtract(result, operand);
-            case MULTIPLY:
-                return multiply(result, operand);
-            case DIVIDE:
-                return divide(result, operand);
-            default:
-                throw new InputMismatchException("해당 연산자 없음");
-        }
-    }
-
-    /**
-     * add는 입력받은 두 정수형 값을 더하여 반환한다.
-     *
-     * @param a 계산을 수행할 정수형 숫자이다.
-     * @param b 계산을 수행할 정수형 숫자이다.
-     * @return 계산된 값을 반환한다.
-     */
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    /**
-     * add는 입력받은 첫번째 정수값에서 두번째 정수값을 뺀 뒤 그 값을 반환한다.
-     *
-     * @param a 계산을 수행할 정수형 숫자이다.
-     * @param b 계산을 수행할 정수형 숫자이다.
-     * @return 계산된 값을 반환한다.
-     */
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-
-    /**
-     * add는 입력받은 두 정수형 값을 하여 반환한다.
-     *
-     * @param a 계산을 수행할 정수형 숫자이다.
-     * @param b 계산을 수행할 정수형 숫자이다.
-     * @return 계산된 값을 반환한다.
-     */
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    /**
-     * divide는 입력받은 첫번째 값을 입력받은 두번째 값으로 나눠 그 값을 반환한다.
-     *
-     * @param a 계산을 수행할 정수형 숫자이다.
-     * @param b 계산을 수행할 정수형 숫자이다.
-     * @return 계산된 값을 반환한다.
-     */
-    public int divide(int a, int b) {
-        try {
-            return a / b;
-        } catch (ArithmeticException e) {
-            throw e;
-        }
+    public int calculate(int result, String operator, int operand) throws ArithmeticException, IllegalArgumentException {
+        return CalculatorType.operate(result, operand, operator);
     }
 }
