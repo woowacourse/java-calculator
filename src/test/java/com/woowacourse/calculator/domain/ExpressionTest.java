bb6ff8aa6fae30d4b79604a5159d5c482962ca50
lxxjn0 <ljy3510@gmail.com>
2020-02-08 17:49:16 +0900

    test : Expression 객체 테스트 수정
    1. ExpressionTest 클래스 DisplayName 추가.
    2. Expression 객체의 calculate 매소드 테스트 추가.

--- src/test/java/com/woowacourse/calculator/domain/ExpressionTest.java
+++ src/test/java/com/woowacourse/calculator/domain/ExpressionTest.java
@@ -2,32 +2,60 @@ package com.woowacourse.calculator.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Expression Test")
 class ExpressionTest {
-	@Test
-	void testTokens() {
-		List<Token> invalidTokens = Arrays.asList(new Number("1"), new Operator("+"), new Operator("+"));
-		assertThatThrownBy(() -> {
-			new Expression(invalidTokens);
-		}).isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("잘못된 식입니다.");
-	}
+    @DisplayName("Expression - Expression() Test")
+    @Test
+    void testExpression() {
+        List<Token> invalidTokens = new ArrayList<>(Arrays.asList(
+                new Number("1"),
+                new Operator("+"),
+                new Operator("+")));
+        assertThatThrownBy(() -> new Expression(invalidTokens))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 연산 식입니다.");
+    }
 
-	@Test
-	void testGetExpression() {
-		List<Token> value = Arrays.asList(new Number("1"), new Operator("+"), new Number("2"));
-		Expression expression = new Expression(value);
-		Stack<Token> actual = expression.getExpression();
-		Stack<Token> expected = new Stack<>();
-		expected.push(new Number("2"));
-		expected.push(new Operator("+"));
-		expected.push(new Number("1"));
-		Assertions.assertThat(actual).isEqualTo(expected);
-	}
+    @DisplayName("Expression - GetExpression() Test")
+    @Test
+    void testGetExpression() {
+        List<Token> value = new ArrayList<>(Arrays.asList(
+                new Number("1"),
+                new Operator("+"),
+                new Number("2")));
+        Expression expression = new Expression(value);
+        Stack<Token> actual = expression.getExpression();
+        Stack<Token> expected = new Stack<>();
+        expected.push(new Number("2"));
+        expected.push(new Operator("+"));
+        expected.push(new Number("1"));
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("Expression - calculate() Test")
+    @Test
+    void testCalculate() {
+        List<Token> value = new ArrayList<>(Arrays.asList(
+                new Number("1"),
+                new Operator("+"),
+                new Number("2"),
+                new Operator("*"),
+                new Number("4"),
+                new Operator("-"),
+                new Number("3")));
+        Expression expression = new Expression(value);
+        int actual = expression.calculate();
+        int expected = 9;
+        assertThat(actual).isEqualTo(expected);
+
+    }
 }
