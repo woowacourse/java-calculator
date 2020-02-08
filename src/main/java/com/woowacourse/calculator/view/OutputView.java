6f6044b09ea1ad9dba7aa2271e3c48c58389a06a
lxxjn0 <ljy3510@gmail.com>
2020-02-08 17:30:45 +0900

    feat : OutputView 클래스 구현
    1. 계산 결과를 출력하기 위한 OutputView 클래스 구현.

---
+++ src/main/java/com/woowacourse/calculator/view/OutputView.java
@@ -0,0 +1,7 @@
+package com.woowacourse.calculator.view;
+
+public class OutputView {
+    public static void printCalculateResult(final int calculateResult) {
+        System.out.println("계산 결과: " + calculateResult);
+    }
+}
