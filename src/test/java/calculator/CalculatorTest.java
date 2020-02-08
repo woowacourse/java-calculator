48d3b3186f7ec95f38eb91a804d374cb67a45fb7
jamie9504 <jamie9504@kakao.com>
2020-02-08 22:24:40 +0900

    컨벤션 맞추기

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,7 +3,6 @@ package calculator;
 import domain.Calculator;
 import domain.Formulas;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
