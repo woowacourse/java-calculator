e7ad466884ac9e0feaea9e4fb6748407339db8ee
jamie9504 <jamie9504@kakao.com>
2020-02-08 18:30:47 +0900

    문자열 계산기 - validation 테스트 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,18 +2,31 @@ package calculator;
 
 import domain.Calculator;
 import domain.Formulas;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
-    @Test
-    void calculateTest() {
-        Calculator calculator = new Calculator(new Formulas("1 + 2"));
-        int result = calculator.stringCalculate();
-        assertThat(result).isEqualTo(3);
-        Calculator calculator2 = new Calculator(new Formulas("2 + 3 * 4 / 2"));
-        result = calculator2.stringCalculate();
-        assertThat(result).isEqualTo(10);
+    @DisplayName("validation 테스트 - 성공편")
+    @ParameterizedTest
+    @CsvSource(value = {"1 + 2, 3"
+            , "2 + 3 * 4 / 2, 10"
+            , "1000 _ 1000, 0"
+            , "100 + + 1, 1001"
+            , "100000000000000000000000000000000000 - 1, 9999"
+            , "+10 - -10, 20"
+            , "100 + 10.0.0, 110"
+    })
+    void calculateParameterTest(String formula, int expected) {
+        try {
+            int result = new Calculator(new Formulas(formula)).stringCalculate();
+            assertThat(result).isEqualTo(expected);
+        } catch (IllegalArgumentException iae) {
+            assertThat(iae.getClass()).isEqualTo(IllegalArgumentException.class);
+        } catch (NullPointerException npe) {
+            assertThat(npe.getClass()).isEqualTo(IllegalArgumentException.class);
+        }
     }
 }
