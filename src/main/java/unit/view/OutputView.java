cbfd21c314656497fb050b55057939b82c4968f1
kouz <lastpuzly@gmail.com>
2020-02-08 13:18:57 +0900

    refactor: 출력 기능 수정
    1. 소수점 밑의 숫자가 존재하지 않을 경우 정수로 출력한다.

--- src/main/java/unit/view/OutputView.java
+++ src/main/java/unit/view/OutputView.java
@@ -2,6 +2,10 @@ package unit.view;
 
 public class OutputView {
 	public static void printResult(final double result) {
-		System.out.println(String.format("계산 결과 : %.3f", result));
+		if (result == (long)result) {
+			System.out.println(String.format("계산 결과 : %d", (long)result));
+			return;
+		}
+		System.out.println(String.format("계산 결과 : %s", result));
 	}
 }
