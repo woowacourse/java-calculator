405355894ffaa2328ef5e039f1413c0a86108da6
Sujin <sujinemilycho@gmail.com>
2020-02-08 16:08:55 +0900

    fix: 입력 문자열 trim

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -7,13 +7,13 @@ public class InputView {
     public static String inputEquation() {
         System.out.println("계산할 수식을 입력해주세요. (숫자와 연산자는 공백으로 구분)");
         System.out.println("예시> 1 + 2");
-        String equation = new Scanner(System.in).nextLine();
+        String equation = new Scanner(System.in).nextLine().trim();
         validate(equation);
         return equation;
     }
 
     private static void validate(String equation) {
-        if (equation.trim().isEmpty()) {
+        if (equation.isEmpty()) {
             OutputView.printMessage_EmptyCase();
             throw new IllegalArgumentException("빈 문자열");
         }
