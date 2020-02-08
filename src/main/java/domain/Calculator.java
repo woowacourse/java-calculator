a0729c0bb590db146a84ff8ad25d41c5d8b55f56
lalize <printf@kakao.com>
2020-02-08 16:11:49 +0900

    refactor: 인덱스 관련한 매직넘버를 클래스 상수로 수정

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -1,12 +1,16 @@
 package domain;
 
 public class Calculator {
+	private static final int FIRST_OPERAND_INDEX = 0;
+	private static final int FIRST_OPERATOR_INDEX = 1;
+	private static final int OPERATOR_INDEX_STEP = 2;
+
 	public static double calculate(String[] tokens) {
 		if (isEmpty(tokens)) {
 			return 0;
 		}
-		double result = Double.parseDouble(tokens[0]);
-		for (int i = 1; i < tokens.length; i += 2) {
+		double result = Double.parseDouble(tokens[FIRST_OPERAND_INDEX]);
+		for (int i = FIRST_OPERATOR_INDEX; i < tokens.length; i += OPERATOR_INDEX_STEP) {
 			Operator operator = Operator.of(tokens[i]);
 			double rightOperand = Double.parseDouble(tokens[i + 1]);
 			result = operator.calculate(result, rightOperand);
