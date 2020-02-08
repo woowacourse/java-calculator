598daba074dddd13c14e578998b2e085fba75a30
minuyim <minu_yim@naver.com>
2020-02-09 03:18:09 +0900

    [chore] util 패키지명을 view로 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,6 @@
 import domain.Calculator;
-import util.InputView;
-import util.OutputView;
+import view.InputView;
+import view.OutputView;
 
 public class Application {
 	public static void main(String[] args) {
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,4 @@
-package util;
+package view;
 
 import java.util.Scanner;
 
