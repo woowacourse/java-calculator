50f889412af1eef8ed7368a91afb38bfde3f3ad5
kouz <lastpuzly@gmail.com>
2020-02-08 12:51:16 +0900

    test: calculate 테스트 작성

--- src/test/java/unit/domain/OperatorTest.java
+++ src/test/java/unit/domain/OperatorTest.java
@@ -1,7 +1,6 @@
 package unit.domain;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,5 +15,25 @@ class OperatorTest {
 			new Operator(invalidValue);
 		}).isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("유효한 연산자 형식이 아닙니다.");
+	}
+
+	@Test
+	void testCalculate() {
+		Operator plus = new Operator("+");
+		final Number one = new Number("1");
+		final Number two = new Number("2");
+
+		final int expected = 3;
+		final int actual = plus.calculate(one, two);
+
+		assertThat(actual).isEqualTo(expected);
+
+		Operator divide = new Operator("/");
+		final Number zero = new Number("0");
+
+		assertThatThrownBy(() -> {
+			divide.calculate(one, zero);
+		}).isInstanceOf(IllegalArgumentException.class)
+			.hasMessage(("0 으로 나눌 수 없습니다."));
 	}
 }
