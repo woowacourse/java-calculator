c3f25dfdde9819850c0cad3ddafaf6c011d76892
lxxjn0 <ljy3510@gmail.com>
2020-02-08 18:04:35 +0900

    test : Operator 객체 테스트 수정
    1. OperatorTest 클래스 DisplayName 추가.
    2. Operator 객체 calculate 매소드 테스트 추가.

--- src/test/java/com/woowacourse/calculator/domain/OperatorTest.java
+++ src/test/java/com/woowacourse/calculator/domain/OperatorTest.java
@@ -4,16 +4,30 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-@DisplayName("Operator test")
+@DisplayName("Operator Test")
 class OperatorTest {
-	@DisplayName("operator test")
-	@Test
-	void testOperator() {
-		final String invalidValue = "%";
-		assertThatThrownBy(() -> {
-			new Operator(invalidValue);
-		}).isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("유효한 연산자 형식이 아닙니다.");
-	}
+    @DisplayName("Operator - Operator() Test")
+    @Test
+    void testOperator() {
+        final String invalidOperator = "%";
+        assertThatThrownBy(() -> new Operator(invalidOperator))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유효한 연산자 형식이 아닙니다.");
+    }
+
+    @DisplayName("Operator - calculate() Test")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "+, 1,2,3",
+            "-, 1,2,-1",
+            "/, 2,1,2",
+            "*, 2,3,6"})
+    void testCalculate(final String inputOperator, final String inputNumber1, final String inputNumber2, final int expected) {
+        Operator operator = new Operator(inputOperator);
+        int actual = operator.calculate(new Number(inputNumber1), new Number(inputNumber2));
+        assertThat(actual).isEqualTo(expected);
+    }
 }
