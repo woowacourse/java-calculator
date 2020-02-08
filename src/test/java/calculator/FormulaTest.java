6567336d403bf8b9cbedbeabeb5869569320e49d
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-02-08 20:57:11 +0900

    test: 홀수번째 인덱스에 숫자 외의 다른 입력이 들어왔을 경우의 테스트 구현

--- src/test/java/calculator/FormulaTest.java
+++ src/test/java/calculator/FormulaTest.java
@@ -29,6 +29,15 @@ public class FormulaTest {
     }
 
     @ParameterizedTest
+    @ValueSource(strings = {"+ 1 2", "1 + as + 4", "4 + *"})
+    void nonOperandInOddIndex_ShouldThrowException(String input) {
+        String[] inputs = input.split(" ");
+        assertThatThrownBy(() -> formula = new Formula(inputs))
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessageMatching("For input string: \".+\"");
+    }
+
+    @ParameterizedTest
     @CsvSource(value = {"1 + 2 / 3:1", "3 + 4 + 5:12", "5.5 - 1.5 / 2:2", "3 * 6:18"}, delimiter = ':')
     void calculateFormula_ShouldCalculateProperly(String input, double expected) {
         String[] inputs = input.split(" ");
