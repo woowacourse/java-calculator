e4c2efa907cbadce3ea87b70c9a6645eb95f01d7
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-02-08 20:53:10 +0900

    test: 짝수번째 인덱스에 사칙연산 부호 외의 다른 입력이 들어왔을 경우의 테스트 구현

--- src/test/java/calculator/FormulaTest.java
+++ src/test/java/calculator/FormulaTest.java
@@ -20,6 +20,15 @@ public class FormulaTest {
     }
 
     @ParameterizedTest
+    @ValueSource(strings = {"1 j 2", "1 ^ 3 + 4", "4 3 4"})
+    void nonOperatorInEvenIndex_ShouldThrowException(String input) {
+        String[] inputs = input.split(" ");
+        assertThatThrownBy(() -> formula = new Formula(inputs))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching(".+ is not valid operator");
+    }
+
+    @ParameterizedTest
     @CsvSource(value = {"1 + 2 / 3:1", "3 + 4 + 5:12", "5.5 - 1.5 / 2:2", "3 * 6:18"}, delimiter = ':')
     void calculateFormula_ShouldCalculateProperly(String input, double expected) {
         String[] inputs = input.split(" ");
