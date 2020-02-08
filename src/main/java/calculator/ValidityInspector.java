470960aac474a314778b0a9324b7423cf0bdb479
giantim <giantim132@gmail.com>
2020-02-08 20:14:04 +0900

    refact : 입력 문자열의 공백 또는 null 검사 메소드를 하나로 통일 / 매직넘버 제거 / checkIsDoubleNumber -> checkCorrectDoubleNumber, checkIsOperator -> checkCorrectOperator 로 메소드 이름 변경

--- src/main/java/calculator/ValidityInspector.java
+++ src/main/java/calculator/ValidityInspector.java
@@ -15,37 +15,34 @@ public class ValidityInspector {
     }
 
     public void isValidInput(String input) {
-        if (isNull(input) || isBlank(input) || isEmpty(input)) {
+        if (isBlank(input) || isNullOrEmpty(input)) {
             throw new IllegalArgumentException("Null or Blank or Empty exception.");
         }
     }
 
-    private boolean isNull(String input) {
-        return input == null;
+    private boolean isNullOrEmpty(String input) {
+        return input.equals("");
     }
 
     private boolean isBlank(String input) {
         return input.equals(" ");
     }
 
-    private boolean isEmpty(String input) {
-        return input.equals("");
-    }
-
     public void isValidSplitedInput(String[] splitedInput) {
-        checkIsDoubleNumber(splitedInput[0]);
+        String firstClause = splitedInput[0];
+        checkCorrectDoubleNumber(firstClause);
         for (int i = 1; i < splitedInput.length; i = i + 2) {
             String operator = splitedInput[i];
-            String number = splitedInput[i + 1];
-            if (operator.equals("/") && number.equals("0")) {
+            String secondClause = splitedInput[i + 1];
+            if (Operator.Division.getValue().equals(operator) && "0".equals(secondClause)) {
                 throw new IllegalArgumentException("Cannot divide zero.");
             }
-            checkIsOperator(operator);
-            checkIsDoubleNumber(number);
+            checkCorrectOperator(operator);
+            checkCorrectDoubleNumber(secondClause);
         }
     }
 
-    private void checkIsDoubleNumber(String s) {
+    private void checkCorrectDoubleNumber(String s) {
         try {
             Double.valueOf(s);
         } catch (Exception e) {
@@ -53,7 +50,7 @@ public class ValidityInspector {
         }
     }
 
-    private void checkIsOperator(String s) {
+    private void checkCorrectOperator(String s) {
         if (!operators.contains(s)) {
             throw new IllegalArgumentException("Operator check exception.");
         }
