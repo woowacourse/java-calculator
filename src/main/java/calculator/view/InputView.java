32bd5bfb8b3c4579d0ca404505ecf17ac60ea49d
Sujin <sujinemilycho@gmail.com>
2020-02-08 16:11:05 +0900

    refactor: 수식 입력 메서드 구조 변경

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -8,14 +8,14 @@ public class InputView {
         System.out.println("계산할 수식을 입력해주세요. (숫자와 연산자는 공백으로 구분)");
         System.out.println("예시> 1 + 2");
         String equation = new Scanner(System.in).nextLine().trim();
-        validate(equation);
-        return equation;
+        return validate(equation);
     }
 
-    private static void validate(String equation) {
+    private static String validate(String equation) {
         if (equation.isEmpty()) {
             OutputView.printMessage_EmptyCase();
             throw new IllegalArgumentException("빈 문자열");
         }
+        return equation;
     }
 }
