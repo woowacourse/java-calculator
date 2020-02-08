d689dc213da69049ec23482eda57d18e486df48d
kuenhwi <kueniyam@naver.com>
2020-02-09 07:17:36 +0900

    refactor: try 문의 범위 수정
    변수의 사용 범위가 try 문 안에 있을 경우 try 문 안에서 초기화

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,20 +4,19 @@ import view.InputView;
 import view.OutputView;
 
 public class Application {
-	public static void main(String[] args) {
-		while (true){
-			String input = InputView.inputExpression();
-			useCalculator(input);
-		}
-	}
+    public static void main(String[] args) {
+        while (true) {
+            String input = InputView.inputExpression();
+            useCalculator(input);
+        }
+    }
 
-	public static void useCalculator(String input) {
-		String result;
-		try {
-			result = Calculator.calculate(input);
-			OutputView.printResult(result);
-		} catch (IllegalArgumentException e) {
-			ErrorView.println(e.getMessage());
-		}
-	}
+    public static void useCalculator(String input) {
+        try {
+			String result = Calculator.calculate(input);
+            OutputView.printResult(result);
+        } catch (IllegalArgumentException e) {
+            ErrorView.println(e.getMessage());
+        }
+    }
 }
