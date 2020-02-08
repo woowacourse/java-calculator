a0840877742aeb448e20fc1ef2fe2e5602653ef2
Andy H. Jung <commaniakr@gmail.com>
2020-02-08 21:10:48 +0900

    test: 오류 상황 테스트 추가

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -2,10 +2,11 @@ package study;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import study.domain.Calculator;
 import study.domain.Operator;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class CalculatorTest {
     @ParameterizedTest
@@ -19,9 +20,20 @@ public class CalculatorTest {
 
     @ParameterizedTest
     @CsvSource(value = {"2 + 3 * 4 / 2,10", "2 + 4 / 2 * 6,18"})
-    void CalculateString(String expression, double expected) {
+    void calculateString(String expression, double expected) {
         Calculator calculator = new Calculator();
         String[] values = expression.split(" ");
         assertThat(calculator.calculateString(values)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"4 * 3 *", "3 3 3", "123*", "/ 5 * 3", "4 / 0", "2*5/4"})
+    void calculateStringError(String expression) {
+        Calculator calculator = new Calculator();
+        String[] values = expression.split(" ");
+        assertThatExceptionOfType(Exception.class)
+                .isThrownBy(() -> {
+                    calculator.calculateString(values);
+                });
     }
 }
