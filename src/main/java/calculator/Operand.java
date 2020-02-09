c83a010c02665691b1663d36e17d3153e0bc800a
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-02-09 16:14:52 +0900

    refactor: Operand 클래스의 최종 초기화를 하나의 생성자에서 처리하게 변경

--- src/main/java/calculator/Operand.java
+++ src/main/java/calculator/Operand.java
@@ -3,12 +3,12 @@ package calculator;
 public class Operand extends FormulaElement {
     private final double operand;
 
-    public Operand(String input) {
-        operand = Double.parseDouble(input);
-    }
-
     public Operand(double operand) {
         this.operand = operand;
+    }
+
+    public Operand(String input) {
+        this(Double.parseDouble(input));
     }
 
     public double getOperand() {
