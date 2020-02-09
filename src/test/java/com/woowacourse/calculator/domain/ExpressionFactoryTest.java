751d7ca1f0b921b805932366fd78cf1f12d56513
lxxjn0 <ljy3510@gmail.com>
2020-02-09 22:58:41 +0900

    refactor : Tokenizer 클래스를 ExpressionFactory 클래스로 변경
    1. Token 인터페이스의 제거로 인해, Tokenizer 클래스를 ExpressionFactory 클래스로 변경.

--- src/test/java/com/woowacourse/calculator/domain/ExpressionFactoryTest.java
+++ src/test/java/com/woowacourse/calculator/domain/ExpressionFactoryTest.java
@@ -3,20 +3,19 @@ package com.woowacourse.calculator.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-@DisplayName("Tokenizer Test")
-class TokenizerTest {
+@DisplayName("ExpressionFactory Test")
+class ExpressionFactoryTest {
     @DisplayName("Tokenize - Tokenize() Test")
     @Test
     void testTokenize() {
         final String inputExpression = "1 + 2";
 
-        final List<Token> actual = Tokenizer.tokenize(inputExpression);
+        final List<Token> actual = ExpressionFactory.generate(inputExpression);
 
         final List<Token> expected = Arrays.asList(new Number("1"), new Operator("+"), new Number("2"));
 
