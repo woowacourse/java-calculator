87ab41eeb6f8000a5459143e7b5b9e069ac0b3f5
KIMSIYOUNG <tldud2404@gmail.com>
2020-02-09 22:10:41 +0900

    refactor : 사용자입력 배열에서 연산자 인덱스를 상수로 분리

--- src/main/java/calculator/TextCalculator.java
+++ src/main/java/calculator/TextCalculator.java
@@ -3,12 +3,14 @@ package calculator;
 public class TextCalculator {
     private static final int STANDARD_NUMBER = 0;
     public static final String BLANK = " ";
+    public static final int OPERATOR_INDEX = 1;
+    public static final int OPERATOR_DELIMITER = 2;
 
     public double run(String input) {
         String[] inputArray = split(input);
 
         double result = Double.parseDouble(inputArray[STANDARD_NUMBER]);
-        for (int i = 1; i < inputArray.length; i += 2) {
+        for (int i = OPERATOR_INDEX; i < inputArray.length; i += OPERATOR_DELIMITER) {
             Operator operator = Operator.of(inputArray[i]);
             double right = Double.parseDouble(inputArray[i+1]);
             result = operator.calculate(result, right);
