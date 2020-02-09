45279b8739d1bfc67b7f565efc26697d1e85bb45
kouz <lastpuzly@gmail.com>
2020-02-09 15:37:13 +0900

    refactor: CalculatorController 삭제, main 메소드에서 로직을 진행

--- src/main/java/unit/Application.java
+++ src/main/java/unit/Application.java
@@ -1,11 +1,16 @@
 package unit;
 
-import unit.controller.CalculatorController;
 import unit.domain.Calculator;
+import unit.domain.Expression;
+import unit.domain.Tokenizer;
+import unit.util.DoubleHandler;
+import unit.view.InputView;
+import unit.view.OutputView;
 
 public class Application {
 	public static void main(String[] args) {
-		Calculator calculator = new Calculator(CalculatorController.inputExpression());
-		CalculatorController.printResult(calculator);
+		String inputLine = InputView.inputExpression();
+		double result = Calculator.calculate(new Expression(Tokenizer.tokenize(inputLine)));
+		OutputView.printResult(DoubleHandler.round(result));
 	}
 }
