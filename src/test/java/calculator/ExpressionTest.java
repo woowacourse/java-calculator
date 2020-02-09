4e71ee8ba29e0b48fbd13db85826b834f115b5fb
김예림 <kiel0103@naver.com>
2020-02-09 22:23:34 +0900

    테스트: Expression 클래스 테스트 클래스 작성
    - Expression 클래스의 생성자에 대한 테스트 메서드 constructor() 작성

---
+++ src/test/java/calculator/ExpressionTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+public class ExpressionTest {
+    final String CORRECT_CASE = "2 + 3 * 2";
+    final String WRONG_CASE = "2 + 3 *2";
+
+    @Test
+    public void constructor() {
+        System.out.println("correct testcase test start");
+        Expression correctTest = new Expression(CORRECT_CASE);
+        System.out.println("correct testcase test end");
+
+        System.out.println("wrong testcase start");
+        Expression wrongTest = new Expression(WRONG_CASE);
+        System.out.println("correct testcase test end");
+    }
+}
