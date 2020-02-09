5a23636cc784e1f0dfd0c76c9a53120a65eb2b5e
Sujin <sujinemilycho@gmail.com>
2020-02-09 16:32:53 +0900

    test: Operators 테스트 코드 업데이트
    모든 테스트 DisplayName 지정
    ParameterizedTest를 이용해 각 연산자별 테스트 수행
    예외 케이스 테스트 추가 - 0으로 나누는 경우 / 지원하지 않는 연산자 입력
    경우

--- src/test/java/calculator/domain/OperatorsTest.java
+++ src/test/java/calculator/domain/OperatorsTest.java
@@ -1,46 +1,59 @@
 package calculator.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.InputMismatchException;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class OperatorsTest {
-
+    @DisplayName("Enum의 valueOf()에 대한 학습 테스트")
     @Test
-    public void valueOf() {
+    void valueOf() {
         String symbol = "PLUS";
-        assertThat(Operators.valueOf(symbol)).isEqualTo(Operators.PLUS);
+        Assertions.assertThat(Operators.valueOf(symbol)).isEqualTo(Operators.PLUS);
     }
 
-    @Test
-    public void hasThis() {
-        String symbol = "+";
-        assertThat(Operators.PLUS.hasThis(symbol)).isTrue();
-
-        symbol = new String("+");
-        assertThat(Operators.PLUS.hasThis(symbol)).isTrue();
+    @DisplayName("각 연산자의 연산 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"PLUS, 4, 2, 6", "MINUS, 4, 2, 2", "MULTIPLY, 4, 2, 8", "DIVIDE, 4, 2, 2"})
+    void operate(Operators operator, int a, int b, int expectedResult) {
+        int result = operator.operate(a, b);
+        assertThat(result).isEqualTo(expectedResult);
     }
 
+    @DisplayName("예외 케이스 테스트: 0으로 나누기 연산을 시도하는 경우")
     @Test
-    public void findOperator() {
-        String symbol = "+";
-        assertThat(Operators.findOperator(symbol)).isEqualTo(Operators.PLUS);
-
-        String notSupportedSymbol = "%";
-        //assertThatThrownBy(() -> Operators.findOperator(notSupportedSymbol))
-        //        .isInstanceOf(InputMismatchException.class);
-        assertThatExceptionOfType(InputMismatchException.class)
-                .isThrownBy(() -> Operators.findOperator(notSupportedSymbol));
-    }
-
-    @Test
-    public void operate() {
-        int result = Operators.PLUS.operate(1, 2);
-        assertThat(result).isEqualTo(3);
-
+    void divideByZero() {
         assertThatThrownBy(() -> Operators.DIVIDE.operate(1, 0))
                 .isInstanceOf(ArithmeticException.class);
+    }
+
+    @DisplayName("입력받은 연산자와 연산자 Enum 기호 일치 여부 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"PLUS,+,true", "MINUS,-,true", "MULTIPLY,*,true", "DIVIDE,/,true", "PLUS,-,false"})
+    void hasThis(Operators operator, String symbol, boolean expectedResult) {
+        assertThat(operator.hasThis(symbol)).isEqualTo(expectedResult);
+    }
+
+    @DisplayName("입력받은 연산자로 연산자 Enum 찾기")
+    @ParameterizedTest
+    @CsvSource(value = {"+, PLUS", "-, MINUS", "*, MULTIPLY", "/, DIVIDE"})
+    void findOperator(String symbol, Operators operator) {
+        assertThat(Operators.findOperator(symbol)).isEqualTo(operator);
+    }
+
+    @DisplayName("예외 케이스 테스트: 지원하지 않는 연산자가 입력된 경우")
+    @Test
+    void notSupportedOperator() {
+        String notSupportedOperator = "%";
+        assertThatThrownBy(() -> Operators.findOperator(notSupportedOperator))
+                .isInstanceOf(InputMismatchException.class)
+                .hasMessageContaining("해당 연산자 없음");
     }
 }
