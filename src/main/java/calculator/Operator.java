a7076f9fd5ff2bab1a03ffe8474f53326e691ff7
김예림 <kiel0103@naver.com>
2020-02-09 23:00:06 +0900

    리팩토링: Operator 클래스의 하드코딩된 부분을 enum을 도입해 수정함
    - 기존에 "+", "-", "*", "/"를 직접 썼던 부분을
    private enum OperaterType 을 도입하여 수정함.

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -3,7 +3,17 @@ package calculator;
 public class Operator {
     private String value;
     private enum OperatorType {
+        ADD("+"), SUBTRACT("-"), MULTIPLY("*"), DIVIDE("/");
 
+        private String operatorType;
+
+        OperatorType(String operatorType) {
+            this.operatorType = operatorType;
+        }
+
+        public String getOperatorType() {
+            return this.operatorType;
+        }
     }
 
     public Operator(String input) {
@@ -11,20 +21,23 @@ public class Operator {
     }
 
     public static boolean isOperator(String input) {
-        return input.equals("+") || input.equals("-") || input.equals("*") || input.equals("/");
+        return input.equals(OperatorType.ADD.getOperatorType())
+                || input.equals(OperatorType.SUBTRACT.getOperatorType())
+                || input.equals(OperatorType.MULTIPLY.getOperatorType())
+                || input.equals(OperatorType.DIVIDE.getOperatorType());
     }
 
     public double operate(double firstValue, double secondValue) {
 
-        if (value.equals("+")) {
+        if (value.equals(OperatorType.ADD.getOperatorType())) {
             return firstValue + secondValue;
         }
 
-        if (value.equals("-")) {
+        if (value.equals(OperatorType.SUBTRACT.getOperatorType())) {
             return firstValue - secondValue;
         }
 
-        if (value.equals("*")) {
+        if (value.equals(OperatorType.MULTIPLY.getOperatorType())) {
             return firstValue * secondValue;
         }
 
