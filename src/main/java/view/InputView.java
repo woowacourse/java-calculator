595f43a7e84c15bb24661ce5a6ad8ef2720fbea4
kuenhwi <kueniyam@naver.com>
2020-02-09 23:56:52 +0900

    refactor: InputView의 scanner를 private로 캡슐화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,7 +3,7 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    public static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static String inputExpression() {
         System.out.println("공백으로 구분하여 식을 입력해주세요(ex. 1 + 2 - 4)");
