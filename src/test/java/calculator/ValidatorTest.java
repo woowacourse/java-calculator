71ed869ba510dc8b80095ccf76e922bccd1f3af0
김효찬 <kom1230@hanyang.ac.kr>
2020-02-09 22:10:47 +0900

    Refactor : Input/Validator 클래스 변경에 따른 유효성 검사 test 통합

--- src/test/java/calculator/ValidatorTest.java
+++ src/test/java/calculator/ValidatorTest.java
@@ -17,40 +17,13 @@ public class ValidatorTest {
     }
 
     private static Stream<String> InputStrings() {
-        return Stream.of(" ", null, "", "1 + 2 + 3");
-    }
-
-    private static Stream<String> SplitedStrings() {
-        return Stream.of("q + w + e", "1+2+3", "+ + 2", "1 abcd 2", "2 * 65 / 0", "1 + 2 + 3");
+        return Stream.of(" ", null, "", "q + w + e", "1+2+3", "+ + 2", "1 abcd 2", "2 * 65 / 0", "1 + 2 + 3");
     }
 
     @ParameterizedTest
     @DisplayName("testing for valid string input")
     @MethodSource("InputStrings")
-    public void isValidInputTest(String notValidInputString) {
-        try {
-            validator.isValidInput(notValidInputString);
-            return;
-        } catch (Exception e) {
-            Assertions.assertThatThrownBy(() -> {
-                validator.isValidInput(notValidInputString);
-            }).isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("Handled Exception");;
-        }
-    }
-
-    @ParameterizedTest
-    @DisplayName("testing for valid string input")
-    @MethodSource("SplitedStrings")
-    public void isValidSplitedInputTest(String notValidSplitedStrings) {
-        String[] splitData = notValidSplitedStrings.split(" ");
-        try {
-            validator.isValidSplitedInput(splitData);
-        } catch (Exception e) {
-            Assertions.assertThatThrownBy(() -> {
-                validator.isValidSplitedInput(splitData);
-            }).isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("Handled Exception");
-        }
+    public void isValidInputTest(String inputString) {
+        Assertions.assertThat(validator.isValidInput(inputString)).isTrue();
     }
 }
