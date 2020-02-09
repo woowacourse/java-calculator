d6768c906c14ed99c4bfb8394134589db0fb0af8
MOON JINJU <mjj920930@gmail.com>
2020-02-09 12:51:27 +0900

    test: SourceForCalculating 클래스의 calculateInputs() 메서드가 동작하는지에 대한 테스트 - 문자열 배열을 인자로 받아 SourceForCalculating 객체를 생성하는 메서드 추가 - 문자열 배열의 피연산자의 개수에 따라 테스트를 별도로 작성

--- src/test/java/calculator/SourceForCalculatingTest.java
+++ src/test/java/calculator/SourceForCalculatingTest.java
@@ -1,17 +1,48 @@
 package calculator;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class SourceForCalculatingTest {
-    private SourceForCalculating sourceForCalculating = new SourceForCalculating();
+    private SourceForCalculating sourceForCalculating;
 
-    @BeforeEach
-    void setUp() {
-        String input = "1 + 2 +";
+    void setUp_SourceForCalculating(String[] inputs) {
+        try {
+            sourceForCalculating = new SourceForCalculating(inputs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void sourceWithTwoOperands_ShouldCalculatePrecisely() {
+        String[] inputs = {"1", "+", "2"};
+        setUp_SourceForCalculating(inputs);
+
+        double result = sourceForCalculating.calculateInputs();
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void sourceWithThreeOperands_ShouldCalculatePrecisely() {
+        String[] inputs = {"1", "+", "2", "/", "3"};
+        setUp_SourceForCalculating(inputs);
+
+        double result = sourceForCalculating.calculateInputs();
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void sourceWithFiveOperands_ShouldCalculatePrecisely() {
+        String[] inputs = {"10", "-", "2", "/", "4", "*", "5"};
+        setUp_SourceForCalculating(inputs);
+
+        double result = sourceForCalculating.calculateInputs();
+        assertThat(result).isEqualTo(10);
     }
 
     @ParameterizedTest
