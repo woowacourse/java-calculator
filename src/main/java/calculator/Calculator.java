1c2a14d1ed6b1e20a0bced24806dbc1bd37f31cb
hwanghe159 <hwanghe159@gmail.com>
2020-02-09 15:40:10 +0900

    입출력 분리 OutputView클래스에 인자로 받는 String을 그대로 출력하는 print()추가 공백을 BLANK상수로 선언하여 대체함

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,12 +1,14 @@
 package calculator;
 
 import view.InputView;
+import view.OutputView;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class Calculator {
+    private static final String BLANK = " ";
     List<String> splittedStrings;
 
     Calculator() {
@@ -14,19 +16,19 @@ public class Calculator {
 
     //테스트용 setter메서드
     public void setSplittedStrings(String input) {
-        splittedStrings = Arrays.asList(input.split(" "));
+        splittedStrings = Arrays.asList(input.split(BLANK));
         checkClusteredElements();
         checkIfStartWithSymbol();
     }
 
     public List<String> enterMathematicalExpression() {
         try {
-            splittedStrings = Arrays.asList(InputView.enterMathematicalExpression().split(" "));
+            splittedStrings = Arrays.asList(InputView.enterMathematicalExpression().split(BLANK));
             checkClusteredElements();
             checkIfStartWithSymbol();
             return splittedStrings;
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            OutputView.print(e.getMessage());
             return enterMathematicalExpression();
         }
     }
@@ -70,7 +72,7 @@ public class Calculator {
                     Operator operator = Operator.findOperatorBySymbol(splittedStrings.get(i));
                     state = operator.operate(state, Double.parseDouble(splittedStrings.get(i + 1)));
                 } catch (IllegalArgumentException e) {
-                    System.out.println(e.getMessage());
+                    OutputView.print(e.getMessage());
                     enterMathematicalExpression();
                 }
             }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void printAnswer(Double input) {
         System.out.println("계산된 결과는 " + input + "입니다");
     }
+
+    public static void print(String string) {
+        System.out.println(string);
+    }
 }
