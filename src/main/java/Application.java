e90b052b745aec172fdc377001ea8ec6efca62b9
icyMojito <dev.icymojito@gmail.com>
2020-02-10 01:59:13 +0900

    기능: Application 클래스 제작
    - 문자열 계산기 프로그램을 실행하기 위해 제작

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,32 @@
-import calculator.Runner;
+import calculator.*;
 
 public class Application {
+    private static Expression expression;
+
     public static void main(String[] args) {
-        Runner runner = new Runner();
-        runner.run();
+
+        while (true) {
+            InputView.askInput();
+            String userInput = InputView.getUserInput();
+            if (isCorrectExpression(userInput)) {
+                break;
+            }
+        }
+
+        double result = Calculator.calculate(expression);
+
+        OutputView.printResult(result);
+    }
+
+    private static boolean isCorrectExpression(String userInput) {
+
+        try {
+            expression = new Expression(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+        return true;
     }
 }
