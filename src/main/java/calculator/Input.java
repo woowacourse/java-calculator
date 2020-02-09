2d44f2131f592a0bd837a1110e3ae1e5ce1c7d76
김효찬 <kom1230@hanyang.ac.kr>
2020-02-09 22:09:12 +0900

    Refactor : 계산식 input 처리를 한 번에 하도록 수정.

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -6,27 +6,12 @@ public class Input {
     private Validator validator = new Validator();
     private Scanner scanner = new Scanner(System.in);
 
-    private String inputString() {
+    public String[] inputFormula() {
         System.out.print("계산식을 입력하시오: ");
         String inputLine = scanner.nextLine();
-        try {
-            validator.isValidInput(inputLine);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return inputString();
+        if (validator.isValidInput(inputLine)) {
+            return inputLine.split(" ");
         }
-        return inputLine;
-    }
-
-    public String[] splitString() {
-        String input = inputString();
-        String[] splitInput = input.split(" ");
-        try {
-            validator.isValidSplitedInput(splitInput);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return splitString();
-        }
-        return splitInput;
+        throw new IllegalArgumentException("잘못된 계산 식입니다.");
     }
 }
