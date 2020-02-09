25dba2371c294b4d59d4859c742d11bd7226f2dc
kouz <lastpuzly@gmail.com>
2020-02-09 15:36:17 +0900

    refactor: OutputView가 출력의 역할만 할 수 있도록 변경, DoubleHandler 생성

--- src/main/java/unit/view/OutputView.java
+++ src/main/java/unit/view/OutputView.java
@@ -1,11 +1,7 @@
 package unit.view;
 
 public class OutputView {
-	public static void printResult(final double result) {
-		if (result == (long)result) {
-			System.out.println(String.format("계산 결과 : %d", (long)result));
-			return;
-		}
-		System.out.println(String.format("계산 결과 : %s", result));
+	public static void printResult(final String result) {
+		System.out.println("계산 결과: " + result);
 	}
 }
