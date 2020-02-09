8b5af789f000fa933aaef6a0ba233155174f18f5
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-02-09 16:29:45 +0900

    refactor: Operator 클래스의 최종 초기화를 하나의 생성자에서 처리하게 변경

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -3,8 +3,12 @@ package calculator;
 public class Operator extends FormulaElement {
     private final OperatorType operator;
 
+    public Operator(OperatorType operator) {
+        this.operator = operator;
+    }
+
     public Operator(String input) {
-        this.operator = OperatorType.createOperator(input);
+        this(OperatorType.createOperator(input));
     }
 
     public double calculate(Operand operand1, Operand operand2) {
