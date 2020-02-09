dea6bdb3e6499be995ca9d4b1e06c9b10e08a416
icyMojito <dev.icymojito@gmail.com>
2020-02-10 01:38:55 +0900

    기능: InputView와 OutputView 클래스 제작
    - 사용자의 입력값/출력값과 관련하여 수정을 쉽게 하기 위해 만듦

---
+++ src/main/java/calculator/OutputView.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class OutputView {
+
+    public static void printResult(double result) {
+        System.out.println("◎ 결과는 " + result + "입니다.");
+    }
+
+}
--- src/main/java/calculator/View.java
+++
@@ -1,7 +0,0 @@
-package calculator;
-
-public class View {
-    public static void printResult(double result) {
-        System.out.println("결과는 " + result + "입니다.");
-    }
-}
