ca68246da56b74845e748a6f7ebe136171f8e091
Seyun <ksy90101@gmail.com>
2020-02-09 13:40:59 +0900

    ADD: 잘못 입력한 사용자를 위한 무엇을 잘못 입력했는지 메시지 추가

--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -7,20 +7,35 @@ public class InputValidator {
     private List<String> operators = new ArrayList<>();
 
     public boolean validate(String input) {
-        if (!validateWithRegex(input) || isZeroDivisionExists(input)) {
-            askAgain();
+        String[] splittedInput = input.split(" ");
+        if (!validateWithRegex(input)) {
+            askAgainBeforeSplit();
             return true;
         }
-        String[] splittedInput = input.split(" ");
+        if (isZeroDivisionExists(input)) {
+            askAgainWithZeroDivision();
+            return true;
+        }
         if (validateAfterSplit(splittedInput)) {
-            askAgain();
+            askAgainAfterSplit();
             return true;
         }
         return false;
     }
 
-    private void askAgain() {
-        System.out.println("잘못된 형식을 입력하셨습니다. 다시 입력해주세요.");
+    private void askAgainAfterSplit() {
+        System.out.println("숫자 또는 연산자가 연속으로 오거나, 숫자와 연산자를 공백을 두지 않고 입력하셨습니다.");
+        System.out.println("다시 입력해주세요.");
+    }
+
+    private void askAgainWithZeroDivision() {
+        System.out.println("또는 0으로 나누는 경우가 존재합니다.");
+        System.out.println("다시 입력해주세요.");
+    }
+
+    private void askAgainBeforeSplit() {
+        System.out.println("숫자(0-9) 또는 연산자(+, -, *, /)를 제외한 다른 문자가 들어있습니다.");
+        System.out.println("다시 입력해주세요.");
     }
 
     private boolean validateWithRegex(String input) {
