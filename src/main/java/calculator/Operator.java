72b2690fa50d75a0e252ba7f6f7d5da01c7680fb
kuenhwi <kueniyam@naver.com>
2020-02-09 10:26:31 +0900

    refactor: Operator enum 리팩토링
    Operator enum의 각 변수들에 기호를 추가하고, 이와 관련된 getOperator 함수를 수정하였다.
    이로써 코드길이가 확실히 줄어들고 읽기 편해졌다.

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -5,35 +5,35 @@ import view.ErrorView;
 import java.util.function.BinaryOperator;
 
 public enum Operator {
-    addition(Double::sum),
-    subtraction(Operator::subtract),
-    multiplication(Operator::multiply),
-    division(Operator::divide);
+    addition(Double::sum, "+"),
+    subtraction(Operator::subtract, "-"),
+    multiplication(Operator::multiply, "*"),
+    division(Operator::divide, "/");
 
     private BinaryOperator<Double> operate;
+    private String symbol;
 
-    Operator(BinaryOperator<Double> operate) {
+    Operator(BinaryOperator<Double> operate, String symbol) {
         this.operate = operate;
+        this.symbol = symbol;
     }
 
-    public static Operator getOperator(String operator) throws IllegalArgumentException {
-        if (operator.equals("+")) {
-            return Operator.addition;
+    public static Operator getOperator(String symbol) throws IllegalArgumentException {
+        for (Operator operator : Operator.values()) {
+            if (operator.getSymbol().equals(symbol)) {
+                return operator;
+            }
         }
-        if (operator.equals("-")) {
-            return Operator.subtraction;
-        }
-        if (operator.equals("*")) {
-            return Operator.multiplication;
-        }
-        if (operator.equals("/")) {
-            return Operator.division;
-        }
+
         throw new IllegalArgumentException(ErrorView.InvalidExpressionErrorStr);
     }
 
     public double calculate(double a, double b) {
         return operate.apply(a, b);
+    }
+
+    private String getSymbol() {
+        return symbol;
     }
 
     private static double subtract(double a, double b) {
