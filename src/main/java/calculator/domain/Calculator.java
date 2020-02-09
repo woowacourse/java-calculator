c0bb1a7714291c2aeb19634927bab04db07ec961
Sujin <sujinemilycho@gmail.com>
2020-02-09 19:53:32 +0900

    refactor: Calculator -> EquationHandler,Equation분리
    1. Calculator객체의 기능 책임 분리
    - EquationHandler: 입력된 수식 트림, 유효성 검사, 분리 및 객체 생성
    - Equation: 수식의 값, 연산자 추출, 계산 기능
    2. 입력받은 문자열 trim() 위치 이동(InputView -> EquationHandler)

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -3,20 +3,14 @@ package calculator.domain;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
-import java.util.Arrays;
 import java.util.InputMismatchException;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Calculator {
-    private static final String DELIMITER = "\\s+";
-    private static final int ZERO = 0;
-
     public void start() {
         while (true) {
             try {
-                Queue<String> equation = inputEquation();
-                OutputView.printResult(calculate(equation));
+                Equation equation = inputEquation();
+                OutputView.printResult(getResult(equation));
                 break;
             } catch (InputMismatchException | ArithmeticException | IllegalArgumentException e) {
                 continue;
@@ -24,39 +18,11 @@ public class Calculator {
         }
     }
 
-    private Queue<String> inputEquation() {
-        String rawEquation = validate(InputView.inputEquation());
-        return handleRawEquation(rawEquation);
+    private Equation inputEquation() {
+        return EquationHandler.handleRawEquation(InputView.inputEquation());
     }
 
-    private static String validate(String rawEquation) {
-        if (rawEquation.isEmpty()) {
-            OutputView.printMessage_EmptyCase();
-            throw new IllegalArgumentException("빈 문자열");
-        }
-        return rawEquation;
-    }
-
-    private Queue<String> handleRawEquation(String inputEquation) {
-        String[] equationElements = inputEquation.split(DELIMITER, ZERO);
-        return new LinkedList<>(Arrays.asList(equationElements));
-    }
-
-    private int calculate(Queue<String> equation) {
-        int result = toNumber(equation.poll());
-        while (!equation.isEmpty()) {
-            Operators operator = Operators.findOperator(equation.poll());
-            result = operator.operate(result, toNumber(equation.poll()));
-        }
-        return result;
-    }
-
-    private int toNumber(String element) {
-        try {
-            return Integer.parseInt(element);
-        } catch (NumberFormatException e) {
-            OutputView.printMessage_InvalidEquationCase();
-            throw e;
-        }
+    private int getResult(Equation equation) {
+        return equation.calculate();
     }
 }
--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -7,6 +7,6 @@ public class InputView {
     public static String inputEquation() {
         System.out.println("계산할 수식을 입력해주세요. (숫자와 연산자는 공백으로 구분)");
         System.out.println("예시> 1 + 2");
-        return new Scanner(System.in).nextLine().trim();
+        return new Scanner(System.in).nextLine();
     }
 }
