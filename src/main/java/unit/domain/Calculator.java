91b8b5e8e3c71f9c09320b3a8e083ae4b0387800
kouz <lastpuzly@gmail.com>
2020-02-09 15:35:08 +0900

    refactor: 역할 수정 및 단순화
    Calculator는 Expression을 계산하여 결과를 도출하는 역할을 한다.

--- src/main/java/unit/domain/Calculator.java
+++ src/main/java/unit/domain/Calculator.java
@@ -1,13 +1,32 @@
 package unit.domain;
 
 public class Calculator {
-	private final Expression expression;
+	private static final int MIN_SIZE_OF_TOKENS = 3;
 
-	public Calculator(Expression expression) {
-		this.expression = expression;
+	public static double calculate(Expression expression) {
+		checkValidation(expression);
+
+		double result = 0;
+		Number number1;
+		Operator operator;
+		Number number2;
+
+		while (expression.size() >= MIN_SIZE_OF_TOKENS) {
+			number1 = (Number)expression.pop();
+			operator = (Operator)expression.pop();
+			number2 = (Number)expression.pop();
+
+			result = operator.calculate(number1, number2);
+
+			expression.push(new Number(Double.toString(result)));
+		}
+
+		return result;
 	}
 
-	public double getResult() {
-		return expression.calculate();
+	private static void checkValidation(Expression expression) {
+		if (expression.size() < MIN_SIZE_OF_TOKENS) {
+			throw new IllegalArgumentException("식은 최소 3자 이상이어야 합니다.");
+		}
 	}
 }
