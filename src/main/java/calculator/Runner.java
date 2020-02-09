2c15e34b3f7eb4e3000568c2b8864826051c0a53
김예림 <kiel0103@naver.com>
2020-02-09 22:05:35 +0900

    리팩토링: Runner 클래스의 expression 필드를 삭제함
    - Runner 클래스의 expression 필드가 불필요한 인스턴스 필드로 보여지므로
    이를 삭제함.
    - expression 필드를 사용하던 메서드들의 구조를 수정함.

--- src/main/java/calculator/Runner.java
+++ src/main/java/calculator/Runner.java
@@ -3,14 +3,13 @@ package calculator;
 import java.util.Scanner;
 
 public class Runner {
-    Expression expression;
-    final int ONE_TIME_INPUT_SUCCESS = 1;
-    final int ONE_TIME_INPUT_FAILED = 2;
-
     public void run() {
+        Expression expression = null;
 
-        // 입력값이 조건에 맞지 않는 경우 사용자에게 다시 입력을 요청함
-        while (oneTimeInput(getInput()) == ONE_TIME_INPUT_FAILED);
+        // 입력값이 올바른 연산식(expression)이 아닐 경우 사용자에게 다시 입력을 요청함
+        do {
+            expression = oneTimeInput(getInput());
+        } while (expression == null);
 
         View.printResult(Calculator.calculate(expression));
     }
@@ -21,15 +20,14 @@ public class Runner {
         return scanner.nextLine();
     }
 
-    private int oneTimeInput(String inputString) {
+    private Expression oneTimeInput(String inputString) {
 
         try {
-            expression = new Expression(inputString);
+            return new Expression(inputString);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return ONE_TIME_INPUT_FAILED;
+            return null;
         }
 
-        return ONE_TIME_INPUT_SUCCESS;
     }
 }
