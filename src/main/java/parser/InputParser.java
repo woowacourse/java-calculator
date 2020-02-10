6293ca886946dc8171be37d574ac34420eb482cf
Tak Hyungmin <milkho11@naver.com>
2020-02-10 15:08:03 +0900

    [refact] InputParser 메서드 리팩토링
    메서드들의 반환값을 ArrayList에서 List타입으로 수정하여
    ArrayList에서 LinkedList 로 바뀌는 경우를 대비
    
    조건식의 가독성을 위해 numberIndex라는 변수를 추가

--- src/main/java/parser/InputParser.java
+++ src/main/java/parser/InputParser.java
@@ -3,6 +3,8 @@ package parser;
 import domain.OperatorGroup;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class InputParser {
     private final static String SPACE = " ";
@@ -11,23 +13,25 @@ public class InputParser {
         return input.split(SPACE);
     }
 
-    public static ArrayList<String> extractNumber(String[] input) {
-        ArrayList<String> numbers = new ArrayList<>();
+    public static List<String> extractNumber(String[] input) {
+        List<String> numbers = new ArrayList<>();
+        int numberIndex = 2;
         for (int i = 0; i < input.length; i++) {
-            if (i % 2 == 0) {
+            if (i % numberIndex == 0) {
                 numbers.add(input[i]);
             }
         }
-        return numbers;
+        return Collections.unmodifiableList(numbers);
     }
 
-    public static ArrayList<OperatorGroup> extractOperator(String[] input) {
-        ArrayList<OperatorGroup> operators = new ArrayList<>();
+    public static List<OperatorGroup> extractOperator(String[] input) {
+        List<OperatorGroup> operators = new ArrayList<>();
+        int numberIndex = 2;
         for (int i = 0; i < input.length; i++) {
-            if (i % 2 != 0) {
+            if (i % numberIndex != 0) {
                 operators.add(OperatorGroup.stringToOperator(input[i]));
             }
         }
-        return operators;
+        return Collections.unmodifiableList(operators);
     }
 }
