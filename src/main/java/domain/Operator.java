4a55d8496b040d60ce8896d58bb3fb2e7aa2fc89
hongbin <khbasd@gmail.com>
2020-02-10 21:25:02 +0900

    fix: throw 처리

--- src/main/java/domain/Operator.java
+++ src/main/java/domain/Operator.java
@@ -20,8 +20,9 @@ public enum Operator {
         calculateFunctionMap.put(Operator.MINUS, (x, y) -> x - y);
         calculateFunctionMap.put(Operator.MULTIPLY, (x, y) -> x * y);
         calculateFunctionMap.put(Operator.DIVISION, (x, y) -> {
-            if (y == 0)
-                new IllegalArgumentException("0으로 나눌 수 없습니다.");
+            if (y == 0) {
+                throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+            }
             return x / y;
         });
         stringOperatorToOperatorMap.put("+", Operator.PLUS);
