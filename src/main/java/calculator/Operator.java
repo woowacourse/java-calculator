1877e7d5ae44e6abb92bdd300806f559d0cfa3ac
김예림 <kiel0103@naver.com>
2020-02-10 20:58:11 +0900

    리팩토링: Operator 클래스 수정
    - 기존의 isOperator와 operate 메서드가
    연산자입력을 if ("+"), if ("-"), ... 의 방식으로 받아서
    불필요한 if 문을 장황하게 사용했는데, 이를 해결하기 위해
    HashMap<String, BiFunction<Double, Double, Double>> operatings 를 도입함.

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import java.util.HashMap;
+import java.util.function.BiFunction;
+
 public class Operator {
     private String value;
+    private static final HashMap<String, BiFunction<Double, Double, Double>> operatings = new HashMap<>();
+
+    static {
+        operatings.put("+", (beforeOperand, afterOperand) -> beforeOperand + afterOperand);
+        operatings.put("-", (beforeOperand, afterOperand) -> beforeOperand - afterOperand);
+        operatings.put("*", (beforeOperand, afterOperand) -> beforeOperand * afterOperand);
+        operatings.put("/", (beforeOperand, afterOperand) -> beforeOperand / afterOperand);
+    }
+
     private enum OperatorType {
         ADD("+"), SUBTRACT("-"), MULTIPLY("*"), DIVIDE("/");
 
@@ -21,26 +33,10 @@ public class Operator {
     }
 
     public static boolean isOperator(String input) {
-        return input.equals(OperatorType.ADD.getOperatorType())
-                || input.equals(OperatorType.SUBTRACT.getOperatorType())
-                || input.equals(OperatorType.MULTIPLY.getOperatorType())
-                || input.equals(OperatorType.DIVIDE.getOperatorType());
+        return operatings.containsKey(input);
     }
 
     public double operate(double firstValue, double secondValue) {
-
-        if (value.equals(OperatorType.ADD.getOperatorType())) {
-            return firstValue + secondValue;
-        }
-
-        if (value.equals(OperatorType.SUBTRACT.getOperatorType())) {
-            return firstValue - secondValue;
-        }
-
-        if (value.equals(OperatorType.MULTIPLY.getOperatorType())) {
-            return firstValue * secondValue;
-        }
-
-        return firstValue / secondValue;
+        return operatings.get(value).apply(firstValue, secondValue);
     }
 }
