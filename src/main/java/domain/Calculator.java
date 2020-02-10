d6f4c5994f1df695d8c2a3dc1fb1c524200290b2
hamliet <hamliet@hanyang.ac.kr>
2020-02-11 00:02:46 +0900

    [mod] 문자열 계산기 - 짝수 체크 클래스 추가, 클래스 변수 static제거 및 필요없는 부분 제거

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -1,33 +1,24 @@
 package domain;
 
+import utils.EvenChecker;
+
 public class Calculator {
     private static final int INDEX_INIT = 1;
-    private static final int EVEN = 0;
-    private static final int ODD = 1;
-    static double returnValue;
-    static String nowSign;
 
     public static double run(String input) {
         String[] values = input.split(" ");
-        returnValue = Double.parseDouble(values[0]);
+        StringValue stringValue = new StringValue(Double.parseDouble(values[0]), "");
         for (int i = INDEX_INIT; i < values.length; i++) {
-            checkStep(i, values[i]);
+            checkStep(i, values[i], stringValue);
         }
-        return returnValue;
+        return stringValue.returnValue;
     }
 
-    private static void checkStep(int i, String value) {
-        if (isEven(i) == true) {
-            returnValue = Operator.calculate(nowSign, returnValue, Double.parseDouble(value));
+    private static void checkStep(int i, String value, StringValue stringValue) {
+        if (EvenChecker.isEven(i)) {
+            stringValue.returnValue = Operator.calculate(stringValue.nowSign, stringValue.returnValue, Double.parseDouble(value));
             return;
         }
-        nowSign = value;
-    }
-
-    private static boolean isEven(int i) {
-        if (i % 2 == EVEN) {
-            return true;
-        }
-        return false;
+        stringValue.nowSign = value;
     }
 }
