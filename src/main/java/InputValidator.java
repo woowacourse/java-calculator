483fe958a615e2c9fbb874aa0eb0ebf4a67ff814
Seyun <ksy90101@gmail.com>
2020-02-10 11:09:28 +0900

    REFACTOR: 예외를 발생시켜 메시지를 보내주고 재입력시 메시지 출력

--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -1,40 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class InputValidator {
-
-    private List<String> numbers = new ArrayList<>();
-    private List<String> operators = new ArrayList<>();
+    private final String ERROR_AFTER_SPLIT_MESSAGE = "숫자 또는 연산자가 연속으로 오거나, 숫자와 연산자를 공백을 두지 않고 입력하셨습니다.";
+    private final String ERROR_ZERO_DIVISION_MESSAGE = " 0으로 나누는 경우가 존재합니다.";
+    private final String ERROR_BEFORE_SPLIT_MESSAGE = "숫자(0-9) 또는 연산자(+, -, *, /)를 제외한 다른 문자가 들어있습니다.";
 
     public boolean isReenterWithInvalidValue(String input) {
         String[] splittedInput = input.split(" ");
         if (!validateWithRegex(input)) {
-            askAgainBeforeSplit();
+            askAgain(ERROR_BEFORE_SPLIT_MESSAGE);
             return true;
         }
         if (isZeroDivisionExists(input)) {
-            askAgainWithZeroDivision();
+            askAgain(ERROR_ZERO_DIVISION_MESSAGE);
             return true;
         }
-        if (validateAfterSplit(splittedInput)) {
-            askAgainAfterSplit();
-            return true;
-        }
-        return false;
+        return validateAfterSplit(splittedInput);
     }
 
-    private void askAgainAfterSplit() {
-        System.out.println("숫자 또는 연산자가 연속으로 오거나, 숫자와 연산자를 공백을 두지 않고 입력하셨습니다.");
-        System.out.println("다시 입력해주세요.");
-    }
-
-    private void askAgainWithZeroDivision() {
-        System.out.println("또는 0으로 나누는 경우가 존재합니다.");
-        System.out.println("다시 입력해주세요.");
-    }
-
-    private void askAgainBeforeSplit() {
-        System.out.println("숫자(0-9) 또는 연산자(+, -, *, /)를 제외한 다른 문자가 들어있습니다.");
+    private void askAgain(String ErrorMessage) {
+        System.out.println(ErrorMessage);
         System.out.println("다시 입력해주세요.");
     }
 
@@ -47,38 +30,36 @@ public class InputValidator {
     }
 
     private boolean validateAfterSplit(String[] input) {
-        List<Boolean> isNumberCheck = new ArrayList<>();
-        List<Boolean> isOperatorCheck = new ArrayList<>();
         for (int i = 0; i < input.length; i++) {
-            separateNumbersAndOperators(input[i], i);
+            try {
+                separateNumbersAndOperatorsCheck(input[i], i);
+            } catch (IllegalArgumentException e) {
+                askAgain(e.getMessage());
+                return true;
+            }
         }
-        for (String number : numbers) {
-            isNumberCheck.add(numberCheck(number));
-        }
-        for (String operator : operators) {
-            isOperatorCheck.add(operatorCheck(operator));
-        }
-        return isNumberCheck.contains(false) || isOperatorCheck.contains(false);
+        return false;
     }
 
-    private void separateNumbersAndOperators(String input, int index) {
+    private void separateNumbersAndOperatorsCheck(String input, int index) throws IllegalArgumentException {
         if (index % 2 == 0) {
-            numbers.add(input);
+            numberCheck(input);
             return;
         }
-        operators.add(input);
+        operatorCheck(input);
     }
 
-    private boolean operatorCheck(String operator) {
-        return !operator.matches("^[0-9]");
+    private void operatorCheck(String operator) {
+        if (!operator.matches("[^0-9]")) {
+            throw new IllegalArgumentException(ERROR_AFTER_SPLIT_MESSAGE);
+        }
     }
 
-    private boolean numberCheck(String number) {
+    private void numberCheck(String number) {
         try {
             Double.parseDouble(number);
         } catch (NumberFormatException e) {
-            return false;
+            throw new IllegalArgumentException(ERROR_AFTER_SPLIT_MESSAGE);
         }
-        return true;
     }
 }
