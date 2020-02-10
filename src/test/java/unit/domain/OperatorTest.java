6206e86497ca5488d93198e654905c103f3d98a4
kouz <lastpuzly@gmail.com>
2020-02-10 21:19:18 +0900

    test: Operator 테스트 작성

--- src/test/java/unit/domain/OperatorTest.java
+++ src/test/java/unit/domain/OperatorTest.java
@@ -1,45 +1,30 @@
 package unit.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Operator test")
 class OperatorTest {
-	@DisplayName("operator test")
 	@Test
-	void testOperator() {
-		final String invalidValue = "%";
-		assertThatThrownBy(() -> {
-			new Operator(invalidValue);
-		}).isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("유효한 연산자 형식이 아닙니다.");
+	void testOperateForPlus() {
+		final Number number1 = new Number(1);
+		final Number number2 = new Number(2);
+		double expected = 3;
+		double actual = Operator.PLUS.operate(number1, number2);
+		assertEquals(expected, actual);
 	}
 
-	@DisplayName("calculate test")
 	@Test
-	void testCalculate() {
-		Operator plus = new Operator("+");
-		final Number one = new Number("1");
-		final Number two = new Number("2");
+	void testIsInForPlus() {
+		final String plus = "+";
+		assertTrue(Operator.isIn(plus));
+	}
 
-		final double expected = 3;
-		final double actual = plus.calculate(one, two);
-
-		assertThat(actual).isEqualTo(expected);
-
-		Operator divide = new Operator("/");
-		final Number zero = new Number("0");
-
-		assertThatThrownBy(() -> {
-			divide.calculate(one, zero);
-		}).isInstanceOf(IllegalArgumentException.class)
-			.hasMessage(("0 으로 나눌 수 없습니다."));
-
-		final double expected2 = 0.5;
-		final double actual2 = divide.calculate(one, two);
-
-		assertThat(actual2).isEqualTo(expected2);
+	@Test
+	void testOfForPlus() {
+		final String plus = "+";
+		final Operator expected = Operator.PLUS;
+		final Operator actual = Operator.of(plus);
+		assertEquals(expected, actual);
 	}
 }
