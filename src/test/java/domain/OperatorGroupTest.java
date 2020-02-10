dc887e4ba8dcad43441b3daa46378d8ab48953a7
Tak Hyungmin <milkho11@naver.com>
2020-02-10 15:53:09 +0900

    [refact] OperatorGroup 메서드 테스트 세분화
    모든 사칙연산 테스트 추가

--- src/test/java/domain/OperatorGroupTest.java
+++ src/test/java/domain/OperatorGroupTest.java
@@ -4,14 +4,38 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class OperatorGroupTest {
 
-    @DisplayName("플러스 연산자 확인 테스트")
+    @DisplayName("OperatorGroup Plus 변환 테스트")
     @Test
-    void isPlus() {
-        OperatorGroup plus = OperatorGroup.PLUS;
-        assertThat(plus.isPlus()).isEqualTo(true);
+    void plusStringToOperator() {
+        String plus = "+";
+        OperatorGroup result = OperatorGroup.stringToOperator(plus);
+        assertThat(result).isEqualTo(OperatorGroup.PLUS);
+    }
+
+    @DisplayName("OperatorGroup Minus 변환 테스트")
+    @Test
+    void minusStringToOperator() {
+        String minus = "-";
+        OperatorGroup result = OperatorGroup.stringToOperator(minus);
+        assertThat(result).isEqualTo(OperatorGroup.MINUS);
+    }
+
+    @DisplayName("OperatorGroup Multiple 변환 테스트")
+    @Test
+    void multipleStringToOperator() {
+        String multiple = "*";
+        OperatorGroup result = OperatorGroup.stringToOperator(multiple);
+        assertThat(result).isEqualTo(OperatorGroup.MULTIPLY);
+    }
+
+    @DisplayName("OperatorGroup DIVIDE 변환 테스트")
+    @Test
+    void stringToOperator() {
+        String divide = "/";
+        OperatorGroup result = OperatorGroup.stringToOperator(divide);
+        assertThat(result).isEqualTo(OperatorGroup.DIVIDE);
     }
 }
